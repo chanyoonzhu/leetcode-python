e6f378ba912786afc146034b9f720799ca8069c4
Chanyoon Zhu <chanyoonzhu@Chanyoons-MacBook-Pro-6.local>
2019-01-20 11:29:44 -0600

    add Kruskal to MST

--- Minimum_Spanning_Tree.py
+++ Minimum_Spanning_Tree.py
@@ -1,3 +1,21 @@
+"""
+629. Minimum Spanning Tree
+Difficulty: Hard
+
+http://www.lintcode.com/en/problem/minimum-spanning-tree/
+
+Given a list of Connections, which is the Connection class (the city name at both ends of the edge and a cost between them), find some edges, connect all the cities and spend the least amount. Return the connects if can connect all the cities, otherwise return empty list.
+
+Notice
+
+Return the connections sorted by the cost, or sorted city1 name if their cost is same, or sorted city2 if their city1 name is also same.
+
+Example
+Gievn the connections = ["Acity","Bcity",1], ["Acity","Ccity",2], ["Bcity","Ccity",3]
+
+Return ["Acity","Bcity",1], ["Acity","Ccity",2]
+"""
+
 def Prim(connections):
 
     def findMin(arr):
@@ -59,9 +77,49 @@ def Prim(connections):
 
     return span
 
+def Kruskal(connections):
+
+    def updateSets(sets, nodeA, nodeB):
+        setA_idx = setB_idx = -1
+        for i in range(len(sets)):
+            if nodeA in sets[i]:
+                setA_idx = i
+            if nodeB in sets[i]:
+                setB_idx = i
+        if setA_idx != -1 and setB_idx != -1: # both are connected to other separate node groups
+            sets[setA_idx].union(sets[setB_idx])
+        elif setA_idx == -1 and setB_idx != -1: # B is connected, A is not
+            sets[setB_idx].add(nodeA)
+        elif setA_idx != -1 and setB_idx == -1: # A is connected, B is not
+            sets[setA_idx].add(nodeB)
+        else: # neither A or B is connected
+            sets.append(set([nodeA, nodeB])) 
+
+    span = []
+    sets = []
+
+    edge_sort = sorted(connections,key=lambda x: x[2])
+    # add edge with min weight to spanning tree
+    for e in edge_sort:
+        # check if nodes at the end of edge belong to the same set (already connected)
+        connected = False
+        for s in sets:
+            if e[0] in s and e[1] in s:
+                connected = True
+                break
+        if not connected:
+            span.append(e)
+            # update sets
+            updateSets(sets, e[0], e[1])
+    # test if all nodes are connected
+    if len(sets) > 1:
+        return []
+
+    return span
+
 graph = [["Acity","Bcity",1], ["Acity","Ccity",2], ["Bcity","Ccity",3]]
-print(Prim(graph))
-        
+# print(Prim(graph))
+print(Kruskal(graph))        
         
         
         
