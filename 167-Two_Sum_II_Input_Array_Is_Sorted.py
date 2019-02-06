6d0eb82f27b6464afeba556f95bb3b23a4a3c5a6
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-02-05 20:44:18 -0600

    add Leetcode 167 Amazon

---
+++ 167-Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers)-1
+        while l < r:
+            sum_ = numbers[l] + numbers[r]
+            if sum_ == target:
+                return [l+1, r+1]
+            elif sum_ > target:
+                r -= 1
+            else:
+                l += 1
