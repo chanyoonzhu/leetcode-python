ba79f23629e64f1740e58955fcd10577762a42e5
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-02-09 17:01:26 -0600

    add Leetcode 129 Amazon

---
+++ 129-Sum_Root_to_Leaf_Numbers.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        """
+        - dfs
+        - better solution: https://leetcode.com/problems/sum-root-to-leaf-numbers/discuss/41383/Python-solutions-(dfs%2Bstack-bfs%2Bqueue-dfs-recursively).
+        """
+        def dfs(root, n):
+            if not root: return n
+            n = n * 10 + root.val
+            if not root.left and not root.right:
+                return n
+            elif root.left and root.right:
+                return dfs(root.left, n) + dfs(root.right, n)
+            elif root.left:
+                return dfs(root.left, n)
+            else:
+                return dfs(root.right, n)
+            
+        return dfs(root, 0)
