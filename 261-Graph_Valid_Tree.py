6584d32f1428c536e045afef18bc60e36de7ec3d
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-02-21 11:03:32 -0600

    update Leetcode 261 Amazon

--- 261-Graph_Valid_Tree.py
+++ 261-Graph_Valid_Tree.py
@@ -1,3 +1,5 @@
+import collections
+
 class Solution(object):
     def validTree(self, n, edges):
         """
@@ -9,30 +11,29 @@ class Solution(object):
         """
         - tree: all nodes are connected, no cycle
         """
-        def dfs(node, prev):
-            if visited[node]:
-                return False
-            else:
-                visited[node] = True
-                for nb in graph[node]:
-                    if nb != prev and not dfs(nb, node):
-                        return False
-                return True
-        
+
         graph = collections.defaultdict(list)
-        nodes = set([i for i in range(n)])
-        visited = [False] * n
-        
-        
         for e in edges:
             graph[e[0]].append(e[1])
             graph[e[1]].append(e[0])
             
-        if not dfs(0, -1): return False
-        for val in visited:
-            if not val:
-                return False
-        return True
+        
+        visited = [False] * n
+        
+        def dfs(graph, n, prev, visited):
+            visited[n] = True
+            for nb in graph[n]:
+                if nb != prev: # don't forget to use prev, can point to where it came from
+                    if visited[nb]:
+                        return False
+                    else:
+                        dfs(graph, nb, n, visited)
+            return True
+        
+                
+        if dfs(graph, 0, -1, visited):
+            return sum(visited) == n
+        return False
         
 
 sl = Solution()
