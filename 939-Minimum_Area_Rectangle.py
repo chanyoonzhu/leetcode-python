eb385ba084d00f1f60571de87dcface64df84686
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-02-28 21:40:55 -0600

    add Leetcode 939-Minimum_Area_Rectangle

---
+++ 939-Minimum_Area_Rectangle.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minAreaRect(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        
+        minArea = float('inf')
+        pointsSet = set([tuple(p) for p in points])
+        size = len(points)
+        
+        for i in range(size):
+            for j in range(i+1, size):
+                if (points[i][0] != points[j][0] and points[i][1] != points[j][1]
+                    and (points[i][0], points[j][1]) in pointsSet and (points[j][0], points[i][1]) in pointsSet):
+                    minArea = min(minArea, abs(points[i][1] - points[j][1]) * abs(points[i][0] - points[j][0]))
+                    
+        return minArea if minArea != float('inf') else 0
