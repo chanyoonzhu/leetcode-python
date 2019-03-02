3554662643464d184434f287f083d497f9bf12f5
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-03-02 09:47:40 -0600

    update Leetcode 849-Maximize_Distance_to_Closest_Person

--- 849-Maximize_Distance_to_Closest_Person.py
+++ 849-Maximize_Distance_to_Closest_Person.py
@@ -4,6 +4,39 @@ class Solution(object):
         :type seats: List[int]
         :rtype: int
         """
+
+        """
+        - O(n), O(n)
+        - calculate left distance and right distance separately
+
+        size = len(seats)
+        maxDist = 0
+        
+        left, right = [0] * size, [0] * size
+        left[0] = right[size-1] = float('inf')
+        leftPtr, rightPtr = 0, size-1
+        
+        for i in range(1, size):
+            if seats[i] == 1:
+                leftPtr = i
+            left[i] = i - leftPtr
+        
+        for i in range(size-2, -1, -1):
+            if seats[i] == 1:
+                rightPtr = i
+            right[i] = rightPtr - i
+        
+        for i in range(0, size):
+            maxDist = max(maxDist, min(left[i], right[i]))
+        
+        return maxDist
+        """
+
+        """
+        - O(n), O(1)
+        - group zeros
+        """
+        
         
         maxZeros = 0
         
