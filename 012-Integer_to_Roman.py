837dbe7a0099339727e73196ed31505f8cc4e16c
Chanyoon Zhu <zhuchanyoon@gmail.com>
2020-11-16 00:22:45 -0700

    Updates 012-Integer_to_Roman

--- 012-Integer_to_Roman.py
+++ 012-Integer_to_Roman.py
@@ -4,25 +4,50 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        sym = [['I', 'V', 'X'], ['X', 'L', 'C'], ['C', 'D', 'M'], ['M']]
-        e, base = 3, 10
-        res = ""
-        while e >= 0:
-            div, mod = divmod(num, base ** e)
-            if div == 0:
-                e -= 1
-                continue
-            else:
-                if div < 4:
-                    res += (sym[e][0] * div)
-                elif div == 4:
-                    res += (sym[e][0] + sym[e][1])
-                elif div == 5:
-                    res += sym[e][1]
-                elif div < 9:
-                    res += (sym[e][1] + sym[e][0] * (div - 5))
-                elif div == 9:
-                    res += (sym[e][0] + sym[e][2])
-            num = mod
-            e -= 1
-        return res
+        """
+        - solution 1
+        - O(1) - The same number of operations is done, regardless of the size of the input
+        - O(1)
+        """
+        result = ''
+        roman_digits = [['I', 'V', 'X'], ['X', 'L', 'C'], ['C', 'D', 'M'], ['M', '', '',]]
+        for one, five, ten in roman_digits:
+            # digit, num = num % 10, num // 10
+            num, digit = divmod(num, 10)
+            curr = ''
+            if digit <= 3:
+                curr += one * digit
+            elif digit == 4:
+                curr += (one + five)
+            elif digit <= 8:
+                curr += (five + one * (digit - 5))
+            elif digit == 9:
+                curr += (one + ten)
+            result = (curr + result)
+        return result
+
+
+        """
+        - solution 2
+        - O(1), O(1)
+        """
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), 
+          (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+        def intToRoman(self, num: int) -> str:
+            roman_digits = []
+            # Loop through each symbol.
+            for value, symbol in digits:
+                # We don't want to continue looping if we're done.
+                if num == 0: break
+                count, num = divmod(num, value)
+                # Append "count" copies of "symbol" to roman_digits.
+                roman_digits.append(symbol * count)
+            return "".join(roman_digits)
+                
+            
+        
+        
+        
+        
+        
