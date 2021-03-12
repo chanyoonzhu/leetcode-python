7eb0a68fb25bae77a38b48fcd08bd7d4e97b8cf3
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-03-11 22:50:49 -0700

    Updates 056-Merge_Intervals

--- 056-Merge_Intervals.py
+++ 056-Merge_Intervals.py
@@ -1,49 +1,18 @@
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
-        """
-        - heap
-        - not recommended
-        
-        if not intervals or len(intervals) == 0:
-            return []
-        
-        import heapq
-        
-        heap = [(interval.start, interval.end) for interval in intervals]
-        heapq.heapify(heap)
-        first = heapq.heappop(heap)
-        res = [Interval(first[0], first[1])]
-        
-        while heap:
-            curr = heapq.heappop(heap)
-            if res[-1].end < curr[0]:
-                res.append(Interval(curr[0], curr[1]))
+    """
+    - sweep lines
+    - O(nlog(n)), -O(n)
+    """
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+        result = []
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            if not result:
+                result.append([start, end])
             else:
-                res[-1].end = max(res[-1].end, curr[1])
-            
-        return res
-        """
-        
-        """
-        -O(nlog(n)), -O(n)
-        """
-        res = []
-        for i in sorted(intervals, key=lambda x:x.start):
-            if res:
-                if res[-1].end < i.start:
-                    res.append(i)
+                if start > result[-1][1]:
+                    result.append([start, end])
                 else:
-                    res[-1].end = max(res[-1].end, i.end)
-            else:
-                res.append(i)
-        return res
+                    result[-1][1] = max(result[-1][1], end)
+        return result
