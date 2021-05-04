1bf1af90cc9af3ce2a9838fe85f6ad9cce24211a
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-05-03 18:38:02 -0600

    Adds 863-All_Nodes_Distance_K_in_Binary_Tree

---
+++ 863-All_Nodes_Distance_K_in_Binary_Tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""
+- dfs - two dfs, one for traverse child, one for provide info for parent
+- O(n), O(n)
+"""
+class Solution(object):
+    def distanceK(self, root, target, K):
+        """
+        :type root: TreeNode
+        :type target: TreeNode
+        :type K: int
+        :rtype: List[int]
+        """
+        self.result = []
+        
+        def dfs(node): # returns distance to its decendant target + 1
+            if not node:
+                return -1
+            if node is target:
+                subtree_add(node, K)
+                return 1
+            l_distance_to_target, r_distance_to_target = dfs(node.left), dfs(node.right)
+            if l_distance_to_target == K or r_distance_to_target == K:
+                self.result.append(node.val)
+                return K + 1
+            if 0 < l_distance_to_target <= K:
+                l_distance_to_target += 1
+                subtree_add(node.right, K - l_distance_to_target)
+                return l_distance_to_target
+            if 0 < r_distance_to_target < K:
+                r_distance_to_target += 1
+                subtree_add(node.left, K - r_distance_to_target)
+                return r_distance_to_target
+            return -1
+            
+        def subtree_add(node, distance):
+            if not node:
+                return
+            if distance == 0:
+                self.result.append(node.val)
+                return
+            subtree_add(node.left, distance - 1)
+            subtree_add(node.right, distance - 1)
+            
+        dfs(root)
+        return self.result
+
+"""
+- graph + bfs : todo
+- O(n), O(n)
+"""
+        
