c53ecfc027b7353c9193aea95d12a2d400f257b7
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-05-05 23:14:51 -0600

    Updates 045-Jump_Game_II

--- 045-Jump_Game_II.py
+++ 045-Jump_Game_II.py
@@ -1,3 +1,28 @@
+"""
+- dynamic programming (bottom-up): dp[i] minimum step to be at i
+- O(n^2), -O(n)
+"""
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [float("inf")] * n
+        
+        dp[0] = 0
+        for i in range(n):
+            if dp[i] != float("inf"):
+                for j in range(i + 1, min(n, i + nums[i] + 1)):
+                    dp[j] = min(dp[j], dp[i] + 1)
+        return dp[-1]
+
+ """
+- O(n)
+- Greedy
+- use prev_reachable_index until it cannot reach current index, then update it to max current reachable. 
+"""
 class Solution(object):
     def jump(self, nums):
         """
@@ -5,34 +30,12 @@ class Solution(object):
         :rtype: int
         """
         
-        """
-        - O(n^2), -O(n)
-        - dynamic programming
-        - dp[i] minimum step to be at i
-        - time limit exceeded
-        
-        dp = [float('inf')] * len(nums)
-        dp[0] = 0
-        
-        for i in range(len(dp)):
-            for j in range(i+1,min(i+nums[i]+1, len(dp))):
-                dp[j] = min(dp[j], dp[i] + 1)
-                
-        return dp[-1]
-        """
-        
-        """
-        - O(n)
-        - Greedy
-        - prevReach indicate farthest x steps can reach, currReach indicates farthest index x + 1 step can reach 
-        """
-        res = 0
-        prevReach = currReach = 0
+        prev_reachable_index = curr_reachable_index = 0
+        result = 0
         
         for i in range(len(nums)):
-            if i > prevReach:
-                prevReach = currReach
-                res += 1
-            currReach = max(currReach, i + nums[i])
-            
-        return res
+            if prev_reachable_index < i:
+                result += 1
+                prev_reachable_index = curr_reachable_index
+            curr_reachable_index = max(curr_reachable_index, i + nums[i])
+        return result
