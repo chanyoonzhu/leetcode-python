655cb55629b640bfdb6d2ea2cdb7839f838f77fb
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-05-30 23:45:25 -0600

    Updates 198-House_Robber

--- 198-House_Robber.py
+++ 198-House_Robber.py
@@ -1,11 +1,10 @@
+"""
+- my initial solution
+- dynamic programming: dp[i] - max stolen until house i (i has to be selected)
+- time O(n)
+- space O(n)
+"""
 class Solution(object):
-
-    """
-    - my initial solution
-    - dynamic programming: dp[i] - max stolen until house i (i has to be selected)
-    - time O(n)
-    - space O(n)
-    """
     def rob(self, nums):
         """
         :type nums: List[int]
@@ -16,36 +15,30 @@ class Solution(object):
             dp[i] = max(dp[i-3] + nums[i-3], dp[i-2] + nums[i-3])
         return max(dp[-1], dp[-2])
 
-    """
-    - dynamic programming: dp[i] - max stolen until house i (i doesn't have to be selected)
-    - time O(n)
-    - space O(n)
-    """
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        t = [0] * (len(nums) + 2)
-        for i in range(2, len(nums)+2):
-            t[i] = max(nums[i-2] + t[i-2], t[i-1])
-        return t[-1]
+"""
+- dynamic programming: dp[i] - max stolen until house i (i doesn't necessarily have to be selected)
+- time O(n)
+- space O(n)
+"""
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 2)
+        for i, num in enumerate(nums):
+            dp[i + 2] = max(dp[i + 1], dp[i] + num)
+        return dp[-1]
 
-    """
-    - dynamic programming with space optimization - only need to track t[i-2] and t[i-1] for previous solution
-    - time O(n)
-    - space O(n)
-    """
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-    
-        prev_max, prev2_max = 0, 0
-        for i in range(len(nums)):
-            curr_max = max(nums[i] + prev2_max, prev_max)
-            prev2_max = prev_max
-            prev_max = max(prev_max, curr_max)
-        return prev_max
+"""
+- dynamic programming with space optimization - only need to track t[i-2] and t[i-1] for previous solution
+- time O(n)
+- space O(1)
+"""
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev_two = [0, 0]
+        result = 0
+        for i, n in enumerate(nums):
+            result = max(prev_two[1], prev_two[0] + n)
+            prev_two = [prev_two[1], result]
+        return result
             
