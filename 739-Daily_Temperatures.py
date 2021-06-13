c945d271954d0135bc10a2e663ad5e9089a47aed
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-06-13 17:22:28 -0600

    Adds 739-Daily_Temperatures

---
+++ 739-Daily_Temperatures.py
@@ -0,0 +1,15 @@
+"""
+- monotonically increasing stack
+- O(n), O(n)
+"""
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        result = [0] * len(temperatures)
+        
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                i_smaller = stack.pop()
+                result[i_smaller] = i - i_smaller
+            stack.append(i)
+        return result
