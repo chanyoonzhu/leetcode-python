e6effec9acbb16ed67e233033f1aab134dacad56
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-06-19 17:59:32 -0600

    Adds 1673-Find_the_Most_Competitive_Subsequence

---
+++ 1673-Find_the_Most_Competitive_Subsequence.py
@@ -0,0 +1,14 @@
+"""
+- monotonically increasing stack
+- O(n), O(n)
+"""
+class Solution:
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        stack = []
+        n = len(nums)
+        for i in range(n):
+            while stack and stack[-1] > nums[i] and i - len(stack) <= n - 1 - k: # do not pop if not enough nums left in array
+                stack.pop()
+            if len(stack) < k: # don't add if have enough
+                stack.append(nums[i])
+        return stack
