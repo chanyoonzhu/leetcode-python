f76eb3b1bc2adf669974b4324e4919450f245b2a
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-07-10 23:33:37 -0700

    Updates 003-Longest_Substring_Without_Repeating_Characters

--- 003-Longest_Substring_Without_Repeating_Characters.py
+++ 003-Longest_Substring_Without_Repeating_Characters.py
@@ -1,33 +1,18 @@
-from collections import defaultdict
-
-class Solution(object):
-    """
-    - sliding window: initial solution
-    """
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        char_to_index = defaultdict(list)
-        left = right = 0
-        longest = 0
+"""
+- sliding window
+- O(n), O(n) or O(min(m, n)) m: size of the alphabet
+"""
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         chars = set()
-        while right < len(s):
-            while right < len(s):
-                char = s[right]
-                char_to_index[char].append(right)
-                if char in chars:
-                    longest = max(longest, len(chars))
-                    dup_prev_pos = char_to_index[char][-2]
-                    for i in range(left, dup_prev_pos):
-                        chars.remove(s[i])
-                    left = dup_prev_pos + 1
-                    break
-                else:
-                    chars.add(char)
-                right += 1
-            right += 1
-            longest = max(longest, len(chars))
-            
-        return longest
+        l = 0
+        result = 0
+        for r, c in enumerate(s):
+            if c not in chars:
+                result = max(result, r - l + 1)
+            else:
+                while c in chars:
+                    chars.remove(s[l])
+                    l += 1
+            chars.add(c)
+        return result
