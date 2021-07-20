67bb411e53b99121203891a0124737a2e56daedd
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-07-19 17:27:44 -0700

    Updates 207-Course_Schedule

--- 207-Course_Schedule.py
+++ 207-Course_Schedule.py
@@ -1,70 +1,51 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        """
-        - O(n)
-        - dfs
-        - intuition: https://blog.csdn.net/fuxuemingzhu/article/details/82951771
-        
-        def dfs(graph, visited, i):
-            if visited[i] == 1: # visited in a previous round of dfs
-                return True
-            if visited[i] == 2: # visited in this round of dfs
-                return False
-            visited[i] = 2 # mark this node visited in this round
-            for nb in graph[i]:
-                if not dfs(graph, visited, nb):
-                    return False
-            visited[i] = 1 # if all neighbors checked no cyle, mark visited in previous rounds
-            return True
-                    
-        # create graph (adjacency list)
+"""
+- dfs
+- O(V + E), O(V + E)
+"""
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        can_take = {}
         graph = collections.defaultdict(list)
-        for u, v in prerequisites:
-            graph[v].append(u)
+        for c, pre in prerequisites:
+            graph[c].append(pre)
             
-        visited = [0] * numCourses # 0 - not visited  1 - visited in previous rounds  2 - visited in this dfs round
+        def dfs(c):
+            if c in can_take:
+                return can_take[c]
+            can_take[c] = False
+            for pre in graph[c]:
+                if not dfs(pre):
+                    return False
+            can_take[c] = True
+            return True
         
-        # for every node
-        for i in range(numCourses):
-            if not dfs(graph, visited, i):
-                return False
-        
+        for c in range(numCourses):
+            if not dfs(c): return False
         return True
-        """
+        # return all(dfs(i) for i in range(numCourses))
         
-        """
-        - BFS
-        - intuition: the start node has to have 0 incoming. remove start node from graph (all its neighbors' incoming nodes -1) and do it recursively
-        """
-        import collections
+"""
+- topological sort with BFS
+- O(V + E), O(V + E)
+- intuition: the start node has to have 0 incoming. remove start node from graph (all its neighbors' incoming nodes -1) and do it recursively
+"""
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = collections.defaultdict(list)
+        degrees = [0] * numCourses
+        for c, pre in prerequisites:
+            edges[pre].append(c)
+            degrees[c] += 1
 
-        graph = collections.defaultdict(list)
-        inNodes = [0] * numCourses
-        
-        for p in prerequisites:
-            graph[p[1]].append(p[0])
-            inNodes[p[0]] += 1
-        
-        q = []
-        curr = -1
-        for i in range(numCourses):
-            for j in range(len(inNodes)):
-                if inNodes[j] == 0:
-                    curr = j
-                    inNodes[j] = -1 # mark as visited
-                    q.append(j)
-            if len(q) == 0: # none of the nodes has incoming = 0
-                return False
-            neighbors = graph[q[0]]
-            del q[0]
-            for nb in neighbors:
-                inNodes[nb] -= 1
-        return True
+        q = [course for course, degree in enumerate(degrees) if not degree]
+        while q:
+            course = q.pop(0)
+            for next_course in edges[course]:
+                degrees[next_course] -= 1
+                if not degrees[next_course]:
+                    q.append(next_course)
+
+        return not sum(degrees)
 
 sl = Solution()
 print(sl.canFinish(3, [[1,0],[1,2],[0,1]]))
