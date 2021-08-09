2f6a9a70f53e1903647745fa99134ae2046d93e2
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-08-08 22:32:13 -0700

    Updates 139-Word_Break

--- 139-Word_Break.py
+++ 139-Word_Break.py
@@ -1,70 +1,46 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        """
-        - O(2^n) - O(2^n)
-        - recursion with memoization
+"""
+- dynamic programming (top-down)
+- O(nw), O(n)
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        N = len(s)
         
-        def inDic(s, start, end, mem):
-            thisS = s[start:end+1]
-            if thisS in mem:
-                return mem[thisS]
-            if thisS in wordDict:
-                mem[thisS] = True
-                return True
-            else:
-                if len(thisS) == 1:
-                    mem[thisS] = False
-                    return False
-                else:
-                    for i in range(start, end + 1):
-                        if inDic(s, start, i, mem) and inDic(s, i+1, end, mem):
-                            mem[thisS] = True
-                            return True
-                        else:
-                            mem[thisS] = False
-                    return False
+        def match(i, word):
+            word_len = len(word)
+            if i + word_len > N: return False
+            for ii in range(word_len):
+                if s[ii + i] != word[ii]:
+                    return False 
+            return True
         
-        words = set(wordDict)
-        mem = {}
-        return inDic(s, 0, len(s)-1, mem)
-        """
-        
-        """
-        - O(nm), O(n)
-        - Dynamic Programming
-        - dp[i] - s[:i+1] can be "breakable"
-        - update function:
-            - for any word in dictionary, if s[:i+1] is equals word or if s[i-len(word)+1:i+1] equals word and dp[i-len(word)] is True(s[:i-len(word)+1] breakable), dp[i] is True
-        """
-        dp = [False] * len(s)
-        
-        for i in range(len(dp)):
+        @lru_cache(None)
+        def dfs(i):
+            nonlocal N
+            if i == N: return True
             for word in wordDict:
-                if (dp[i-len(word)] or i-(len(word)-1) == 0) and s[i-len(word)+1:i+1] == word:
-                    dp[i] = True
+                if match(i, word) and dfs(i + len(word)):
+                    return True
+            return False
+        
+        return dfs(0)
+   
+"""
+- dynamic programming (bottom-up)
+- O(nw), O(n)
+"""    
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        N = len(s)
+        dp = [False] * (N + 1)
+        dp[0] = True
+        
+        for i in range(N):
+            for w in wordDict:
+                w_len = len(w)
+                if i + 1 >= w_len and s[i + 1 - w_len:i + 1] == w and dp[i + 1 - w_len]:
+                    dp[i + 1] = True
                     break
-        return dp[-1]
-
-        """
-        - O(n^2)
-        - dp
-        """
-        wordSet = set(wordDict)
-        dp = [False] * len(s)
-        
-        for i in range(len(s)):
-            if s[:i+1] in wordSet:
-                dp[i] = True
-            else:
-                for j in range(len(s)):
-                    if dp[j] and s[j+1:i+1] in wordSet:
-                        dp[i] = True
-        
         return dp[-1]
 
 # Questions: Does case matter?
