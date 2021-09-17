a56181ec00bb34fe9ae4d64521f26f467303a131
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-09-16 21:58:39 -0700

    Adds 444-Sequence_Reconstruction

--- 444-Sequence_Reconstruction.py
+++ 444-Sequence_Reconstruction.py
@@ -37,3 +37,54 @@ class Solution:
                 if not degrees[indexes[nei]]:
                     q.append(indexes[nei])
         return org_i == N
+
+"""
+- Logic (with hashmaps)
+- Must satisfy 2 conditions: 1. Every sequence in seqs should be a subsequence in org; 2.Every 2 consecutive elements in org should be consecutive elements in some sequence from seqs
+"""
+class Solution:
+    def sequenceReconstruction(self, org: List[int], seqs: List[List[int]]) -> bool:
+        
+        if not seqs: return False # edge case: seqs can be []
+        
+        indexes_org = {n: i for i, n in enumerate(org)}
+        
+        indexes_seq = collections.defaultdict(list)
+        for i, seq in enumerate(seqs):
+            for j in range(len(seq)):
+                indexes_seq[seq[j]].append((i, j)) # prepares for condition 2
+                if seq[j] not in indexes_org or j > 0 and indexes_org[seq[j]] <= indexes_org[seq[j - 1]]: # testing condition 1
+                    return False
+        
+        # testing condition 2
+        for i in range(1, len(org)):
+            prev, cur, is_connected = org[i - 1], org[i], False
+            for ii, jj in indexes_seq[prev]:
+                if jj < len(seqs[ii]) - 1 and seqs[ii][jj + 1] == cur:
+                    is_connected = True
+            if not is_connected: return False
+        return True
+
+"""
+- Logic (with hashmaps) - simplifies condition 2 testing
+- Must satisfy 2 conditions: 1. Every sequence in seqs should be a subsequence in org; 2.Every 2 consecutive elements in org should be consecutive elements in some sequence from seqs
+"""
+class Solution:
+    def sequenceReconstruction(self, org: List[int], seqs: List[List[int]]) -> bool:
+        
+        if not seqs: return False
+        
+        indexes_org = {n: i for i, n in enumerate(org)}
+        
+        pairs = set()
+        
+        for i, seq in enumerate(seqs):
+            for j in range(len(seq)):
+                if j > 0: pairs.add((seq[j - 1], seq[j])) # prepares for condition 2
+                if seq[j] not in indexes_org or j > 0 and indexes_org[seq[j]] <= indexes_org[seq[j - 1]]: # testing condition 1
+                    return False
+        
+        # testing condition 2
+        for i in range(1, len(org)):
+            if (org[i - 1], org[i]) not in pairs: return False
+        return True
