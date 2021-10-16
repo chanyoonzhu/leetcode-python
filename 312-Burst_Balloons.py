91f5d9034721aa9d216e7cbef9b7a557c91bb0cd
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-10-15 23:05:12 -0700

    Revisits 312-Burst_Balloons

--- 312-Burst_Balloons.py
+++ 312-Burst_Balloons.py
@@ -3,22 +3,23 @@
 """
 """
 - dp top-down: 
-    dp[i][j] = maximum coins bursting subset nums[i:j] (excluding i and j) 
-    dp[i][j] = max(dp[l][r], dp[l][k] (popped) + dp[k][r] (popped) + nums[l] * nums[k] * nums[r] (to pop at this round))
+    dp[i][j] = maximum coins bursting subset nums[i:j] (including i and j) 
 - O(n^3), O(n^2)
-- exceeds time limit 
 """
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        memo = {}
         nums = [1] + nums + [1]
-        n = len(nums)
-        def dfs(l, r):
-            if (l, r) not in memo:
-                memo[l, r] = max([dfs(l, k) + dfs(k, r) + nums[l] * nums[k] * nums[r] for k in range(l + 1, r)] or [0])
-            return memo[l, r]
-        return dfs(0, len(nums) - 1)
-    
+        
+        @lru_cache(None)
+        def dp(left, right):
+            if left > right: return 0
+            cost = 0
+            # key: k is the LAST ballon to pop between [left, right], not the first
+            for k in range(left, right + 1):
+                cost = max(cost, dp(left, k-1) + nums[left-1] * nums[k] * nums[right+1] + dp(k+1, right))
+            return cost
+        
+        return dp(1, len(nums) - 2)
 
 """
 - dp bottom-up: dp[i][j] = maximum coins bursting subset nums[i:j] (excluding i and j)
@@ -29,9 +30,9 @@ class Solution:
         nums = [1] + nums + [1]
         n = len(nums)
         dp = [[0] * n for _ in range(n)]
-        for sub_size in range(2, n):
-            for l in range(n - sub_size):
-                r = l + sub_size
+        for diff in range(2, n):
+            for l in range(n - diff):
+                r = l + diff
                 for k in range(l + 1, r):
                     dp[l][r] = max(dp[l][r], dp[l][k] + dp[k][r] + nums[l] * nums[k] * nums[r])
         return dp[0][n - 1]
