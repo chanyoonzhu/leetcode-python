278989491eb5733fe1863b27124de6230358ca48
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-10-18 10:57:56 -0700

    Revisits 413-Arithmetic_Slices

--- 413-Arithmetic_Slices.py
+++ 413-Arithmetic_Slices.py
@@ -1,40 +1,58 @@
 """
-- dynamic programming
-- O(n), O(n)
+- dynamic programming (top-down)
+- dp[i] - the length of the arithmetic subarray
 """
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        N = len(nums)
-        if N < 3: return 0
         
-        dp = [0] * N
-        prev_diff = nums[1] - nums[0]
-        for i in range(1, N - 1):
-            cur_diff = nums[i+1] - nums[i]
-            if prev_diff == cur_diff:
-                dp[i+1] = dp[i] + 1 # smart
-            else:
-                prev_diff = cur_diff
-        return sum(dp)
+        @lru_cache(None)
+        def dp(i):
+            if i < 2: return 0
+            
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                return 1 + dp(i - 1)
+            return 0
+          
+        result = 0
+        for i in range(len(nums)):
+            seq_len = dp(i)
+            result = (result + seq_len)
+        return result
 
 """
-- dynamic programming (space optimized)
+- dynamic programming (bottom-up)
 - O(n), O(n)
 """
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+
+        N = len(nums)
+        dp = [0] * N
+        result = 0
+        for i in range(2, N):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp[i] = 1 + dp[i-1]
+                result += dp[i] # length is also the number of subarrays
+        return result
+
+"""
+- dynamic programming (bottom-up: space optimized)
+- O(n), O(1)
+"""
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+
         N = len(nums)
         if N < 3: return 0
         
+        diff, prev = nums[1] - nums[0], 0
         result = 0
-        count = 0
-        prev_diff = nums[1] - nums[0]
-        for i in range(1, N - 1):
-            cur_diff = nums[i+1] - nums[i]
-            if prev_diff == cur_diff:
-                count += 1
-                result += count
+        for i in range(2, N):
+            cur_diff = nums[i] - nums[i-1]
+            if cur_diff == diff:
+                prev += 1
+                result += prev
             else:
-                prev_diff = cur_diff
-                count = 0
+                diff = cur_diff
+                prev = 0
         return result
