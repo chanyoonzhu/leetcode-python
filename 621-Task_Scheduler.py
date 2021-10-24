c86112cc5c6bbc1d92d237c8cc64e5509bb1dbd5
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-10-24 16:28:11 -0700

    Updates 358-Rearrange_String_k_Distance_Apart 621-Task_Scheduler

--- 621-Task_Scheduler.py
+++ 621-Task_Scheduler.py
@@ -1,59 +1,66 @@
 """
+- similar: 358-Rearrange String k Distance Apart
+"""
+"""
 Clarification Questions
 Q: Does case matter?
 A: Only A-Z, upper case
 """
 
+"""
+- priority queue
+- O(nlogn, O(n))
+"""
 class Solution:
-    """
-    - Greedy with heap:
-    - O(n-total), O(n-total) where total = 26, so is actually O(1), O(1)
-    - Algorithm:
-    1. count max possible idle time: most frequent letter (count - 1) * n # understanding this is crucial to solving the problem
-    2. then calculating real idle time by removing idle time that is filled up with tasks
-    3. adding real idle time to number of total tasks to get the final time needed
-    """
-    def leastInterval(self, tasks, n):
-        task_counts = [0] * 26
-        for task in tasks:
-            task_counts[ord(task) - ord('A')] += 1
-        task_counts.sort()
-
-        task_max = task_counts.pop()
-        idle_time = (task_max - 1) * n  # max possible idle time
+    def leastInterval(self, tasks: List[str], n: int) -> int:
         
-        while task_counts and idle_time > 0:
-            idle_time -= min(task_counts.pop(), task_max - 1) # min handles if current task is same size as task_max, it would only take task_max - 1 idle time
-
-        idle_time = max(0, idle_time) # idle time cannot be less than zero
-
-        return idle_time + len(task)
+        idle_time = 0
+        counts = [(-count, c) for c, count in Counter(tasks).items()]
+        heapq.heapify(counts)
+        
+        while counts:
+            is_last_round = True
+            idle_time_cur_round = 0
+            cooling = []
+            for i in range(n + 1):
+                if counts:
+                    cooling.append((heapq.heappop(counts)))
+                    if cooling[i][0] < -1: is_last_round = False
+                else: 
+                    idle_time_cur_round += 1
+            
+            if is_last_round: 
+                break
+            else:
+                idle_time += idle_time_cur_round
+             
+            for count, c in cooling:
+                if count < -1:
+                    heapq.heappush(counts, (count + 1, c))
+                    
+        return len(tasks) + idle_time
 
     
-    """
-    - Math: intuition see image: https://leetcode.com/problems/task-scheduler/solution/
-    - O(n-total), O(n-total) where total = 26, so is actually O(1), O(1)
-    - Algorithm:
-    1. The maximum number of tasks is 26. Let's allocate an array frequencies of 26 elements to keep the frequency of each task.
-    2. Iterate over the input array and store the frequency of task A at index 0, the frequency of task B at index 1, etc.
-    3. Find the maximum frequency: f_max = max(frequencies).
-    4. Find the number of tasks which have the max frequency: n_max = frequencies.count(f_max).
-    5. If the number of slots to use is defined by the most frequent task, it's equal to (f_max - 1) * (n + 1) + n_max.
-    6. Otherwise, the number of slots to use is defined by the overall number of tasks: len(tasks).
-    7. Return the maximum of these two: max(len(tasks), (f_max - 1) * (n + 1) + n_max).
-    """
+
+"""
+- greedy
+- O(n) + O(26log26) = O(n), O(n)
+- Algorithm:
+1. The maximum number of tasks is 26. Let's allocate an array frequencies of 26 elements to keep the frequency of each task.
+2. Iterate over the input array and store the frequency of task A at index 0, the frequency of task B at index 1, etc.
+3. Find the maximum frequency: f_max = max(frequencies).
+4. Find the number of tasks which have the max frequency: n_max = frequencies.count(f_max).
+5. If the number of slots to use is defined by the most frequent task, it's equal to (f_max - 1) * (n + 1) + n_max.
+6. Otherwise, the number of slots to use is defined by the overall number of tasks: len(tasks) => when n is small
+7. Return the maximum of these two: max(len(tasks), (f_max - 1) * (n + 1) + n_max).
+"""
+class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # frequencies of the tasks
-        frequencies = [0] * 26
-        for t in tasks:
-            frequencies[ord(t) - ord('A')] += 1
-
-        # max frequency
-        f_max = max(frequencies)
-        # count the most frequent tasks
-        n_max = frequencies.count(f_max)
-
-        return max(len(tasks), (f_max - 1) * (n + 1) + n_max)
+        counter = Counter(tasks)
+        max_count = max(counter.values())
+        max_count_n = list(counter.values()).count(max_count)
+        
+        return max(len(tasks), (max_count - 1) * (n + 1) + max_count_n) # bucket number * bucket size (except last bucket) + last_bucket_size (see Problem 358),
 
         
         
