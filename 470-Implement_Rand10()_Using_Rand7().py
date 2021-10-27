a28a153122468eeec641c097c56eb0732234aad6
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-10-26 22:58:02 -0700

    Revisits 470-Implement_Rand10\(\)_Using_Rand7\(\)

--- 470-Implement_Rand10()_Using_Rand7().py
+++ 470-Implement_Rand10()_Using_Rand7().py
@@ -3,8 +3,9 @@
 # @return a random integer in the range 1 to 7
 
 """
-- intuition: get a random number 0-39, then % 10 + 1
+- intuition: rand7() -> rand(49) - 1 -> rand(40), then rand(40) % 10 + 1
 - O(1), O(1)
+- https://leetcode.com/problems/implement-rand10-using-rand7/discuss/150301/Three-line-Java-solution-the-idea-can-be-generalized-to-%22Implement-RandM()-Using-RandN()%22
 """
 class Solution:
     def rand10(self):
@@ -13,5 +14,5 @@ class Solution:
         """
         result = 40
         while result >= 40: 
-            result = 7 * (rand7() - 1) + (rand7() - 1) #  get a random number 0 - 48
+            result = 7 * (rand7() - 1) + (rand7() - 1) #  get a uniform random number 0 - 48, note: rand7() * rand7() is not uniform 
         return result % 10 + 1
