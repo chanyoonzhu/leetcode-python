043bd163aef8730c039f1c8181e6477d96b405ab
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-10-30 20:33:13 -0700

    Adds 953-Verifying_an_Alien_Dictionary

---
+++ 953-Verifying_an_Alien_Dictionary.py
@@ -0,0 +1,24 @@
+"""
+- array sorting + hashmap
+"""
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ind = {c: i for i, c in enumerate(order)}
+        for a, b in zip(words, words[1:]):
+            if len(a) > len(b) and a[:len(b)] == b:
+                return False
+            for s1, s2 in zip(a, b):
+                if ind[s1] < ind[s2]:
+                    break
+                elif ind[s1] > ind[s2]:
+                    return False
+        return True
+
+"""
+- short version
+"""
+class Solution:
+    def isAlienSorted(self, words, order):
+        m = {c: i for i, c in enumerate(order)}
+        words = [[m[c] for c in w] for w in words]
+        return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
