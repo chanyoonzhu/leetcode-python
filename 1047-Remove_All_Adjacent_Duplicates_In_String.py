f3711669909bf1bec266eaad97799a2f0e163cbd
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-11-18 23:33:41 -0800

    Adds 1047-Remove_All_Adjacent_Duplicates_In_String

---
+++ 1047-Remove_All_Adjacent_Duplicates_In_String.py
@@ -0,0 +1,13 @@
+"""
+- stack
+- O(n), O(n)
+"""
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if not stack or c != stack[-1]:
+                stack.append(c)
+            else:
+                stack.pop()
+        return ''.join(stack) 
