ea580ca5046011711d275b7fb7942705f0ba01d9
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-11-21 22:31:15 -0800

    Updates 973-K_Closest_Points_to_Origin

--- 973-K_Closest_Points_to_Origin.py
+++ 973-K_Closest_Points_to_Origin.py
@@ -1,31 +1,75 @@
-class Solution(object):
-    def kClosest(self, points, K):
-        """
-        :type points: List[List[int]]
-        :type K: int
-        :rtype: List[List[int]]
-        """
+"""
+- priority queue
+- O(klogn)， O(n)
+"""
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        """
-        - O(n), O(n)
-        """
+        N = len(points)
         
-        def getDistance(pointA, pointB):
-            return (pointA[0] - pointB[0]) ** 2 + (pointA[1] - pointB[1]) ** 2
+        heap = [(self.getDistSquared(points[i]), i) for i in range(N)]
+        heapq.heapify(heap)
+        return [points[i] for _, i in heapq.nsmallest(k, heap)]
         
-        origin = [0, 0]
+    def getDistSquared(self, point: List[int]) -> int:
+        x, y = point
+        return x ** 2 + y ** 2
+
+"""
+- priority queue
+- O(nlogk)， O(k)
+"""
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        import heapq
-        heap = []
+        N = len(points)
         
-        for point in points:
-            heap.append((-getDistance(point, origin), point))
-            
+        heap = [(-self.getDistSquared(points[i]), i) for i in range(k)]
         heapq.heapify(heap)
         
-        while len(heap) > K:
+        for i in range(k, N):
+            heapq.heappush(heap, (-self.getDistSquared(points[i]), i))
             heapq.heappop(heap)
             
-        res = [p[1] for p in heap]
+        return [points[i] for _, i in heap]
         
-        return res
+    def getDistSquared(self, point: List[int]) -> int:
+        x, y = point
+        return x ** 2 + y ** 2
+    
+"""
+- quick select
+- O(n), O(n)
+"""
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        N = len(points)
+        dists = [(self.getDistSquared(points[i]), i) for i in range(N)]
+        
+        l, r = 0, N - 1
+        pivot_index = r
+        while l <= r:
+            pivot_index = self.partition(dists, l, r)
+            if pivot_index == k - 1:
+                return [points[i] for _, i in dists[:k]]
+            if pivot_index < k - 1:
+                left = pivot_index + 1
+            else:
+                right = pivot_index - 1        
+    
+    
+    def getDistSquared(self, point: List[int]) -> int:
+        x, y = point
+        return x ** 2 + y ** 2
+
+    
+    def partition(self, dists, l, r):
+        end = dists[r][0]
+        idx = l
+        for i in range(l, r):
+            if dists[idx][0] <= end:
+                dists[idx], dists[i] = dists[i], dists[idx]
+                idx += 1
+        dists[idx], dists[r] = dists[r], dists[idx]
+        return idx
