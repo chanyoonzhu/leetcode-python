122ce66b7ba115175c7fe9c139916f60deede9bc
Chanyoon Zhu <zhuchanyoon@gmail.com>
2021-12-02 22:50:31 -0800

    Updates 503-Next_Greater_Element_II

--- 503-Next_Greater_Element_II.py
+++ 503-Next_Greater_Element_II.py
@@ -1,9 +1,33 @@
 """
 - similar question: 496. Next Greater Element I
+- difference: circular, numbers not unique
 """
 
 """
-- monotonically increasing stack
+- brute force
+- O(n^2), O(1)
+"""
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        res = []
+        
+        for i in range(n):
+            x = nums[i]
+            greater_found = False
+            for ii in range(n):
+                j = (i + ii) % n
+                if nums[j] > x:
+                    greater_found = True
+                    res.append(nums[j])
+                    break
+            if not greater_found:
+                res.append(-1)
+        return res
+
+"""
+- monotonically decreasing stack
 - O(n), O(n)
 """
 class Solution:
@@ -20,16 +44,19 @@ class Solution:
         return result
 
 """
-- monotonically increasing stack: small space improvement - save index in stack only
+- monotonically decreasing stack: small space improvement - save index in stack only
 - O(n), O(n)
 """
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        stack = []
-        result = [-1] * n
-        for i in list(range(n)) * 2:
-            while stack and nums[i] > nums[stack[-1]]:
-                result[stack.pop()] = nums[i]
-            stack.append(i)
-        return result
+        
+        N = len(nums)
+        stack = [] # mono-decreasing
+        res = [-1] * N
+        
+        for i in range(N * 2): # for circular
+            idx = i % N
+            while stack and nums[idx] > nums[stack[-1]]:
+                res[stack.pop()] = nums[idx]
+            stack.append(idx)
+        return res
