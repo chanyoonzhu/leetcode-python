2cf76948c4e5b0f143e9de31486e30ced8ac92c0
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-01-04 22:27:46 -0800

    Revisits 042-Trapping_Rain_Water

--- 042-Trapping_Rain_Water.py
+++ 042-Trapping_Rain_Water.py
@@ -1,3 +1,8 @@
+"""
+- greedy (two pointers)
+- intuition: move the index on the shorter side towards middle (water will always be trapped as it moves)
+- O(n), O(1)
+"""
 class Solution(object):
     def trap(self, height):
         """
@@ -9,9 +14,7 @@ class Solution(object):
         r = rptr = len(height) - 1
         total = 0
         
-        while l < r:
-            lower = l if height[l] < height[r] else r
-            
+        while l < r:            
             if height[l] < height[r]:
                 while lptr < r and height[lptr] <= height[l]:
                     total += (height[l] - height[lptr])
@@ -33,13 +36,13 @@ class Solution(object):
 """
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []
+        stack = [] # mono-decreasing
         result = 0
-        for i, h in enumerate(height):
+        for right_i, h in enumerate(height):
             while stack and h >= height[stack[-1]]:
                 min_height = height[stack.pop()]
                 if not stack: break # no elevation on the left side to trap water with
-                min_i = stack[-1]
-                result += (min(h, height[min_i]) - min_height) * (i - 1 - min_i) # height diff * width
-            stack.append(i)
+                left_i = stack[-1]
+                result += (min(h, height[left_i]) - min_height) * (right_i - 1 - left_i) # height diff * width
+            stack.append(right_i)
         return result
