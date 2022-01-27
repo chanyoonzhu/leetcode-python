4fddc62030595628fba463ac25ed845e3ad11f14
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-01-26 23:48:08 -0800

    Updates 1405-Longest_Happy_String

--- 1405-Longest_Happy_String.py
+++ 1405-Longest_Happy_String.py
@@ -1,5 +1,38 @@
 """
 - greedy
+- intuition: https://leetcode.com/problems/string-without-aaa-or-bbb/discuss/508543/APPLES-and-BANANAS-solution-(with-picture)
+"""
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        res = [] # groups of chars, each group abides by the rule
+        chars = sorted(zip([a, b, c], ['a', 'b', 'c']), reverse=True)
+        max_count, max_c = chars[0]
+        mid_count, mid_c = chars[1]
+        min_count, min_c = chars[2]
+        
+        res = [max_c + max_c] * (max_count // 2) + [max_c] * (max_count % 2) # put all high freq chars, two per group
+        
+        i = 0
+		# Now delimit existing groups with mid/low freq chars
+        max_i = i # index of the last high freq chars group that got delimited with at least one mid/low freq chars, need to discard groups not delimited since once they got squashed together there'll be 3 or more.
+        # put all mid/low freq chars, one at a time in a round robin fashion
+        for _ in range(mid_count): 
+            if i == len(res):
+                i = 0
+            res[i] += mid_c
+            i += 1
+            max_i = max(i, max_i)
+        for _ in range(min_count):
+            if i == len(res):
+                i = 0
+            res[i] += min_c
+            i += 1
+            max_i = max(i, max_i)
+        
+        return ''.join(res[:max_i+1])
+
+"""
+- greedy
 """
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
@@ -9,7 +42,8 @@ class Solution:
         chars = sorted(zip([a, b, c], ['a', 'b', 'c']), reverse=True)
         max_count, max_c = chars[0]
         mid_count, mid_c = chars[1]
-        min_count, min_c = chars[2]
+        min_count, min_c = ch
+        ars[2]
 		
         # Stage 1: consume all mid_c
         # consume one char with max count and one with mid count per group, until all mid chars are all used - now only has max and min chars left
