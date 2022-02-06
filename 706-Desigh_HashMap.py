072dbb0146409b78a31b6ea718013d72fbdb1cbe
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-06 13:46:00 -0800

    Updates 706-Desigh_HashMap

--- 706-Desigh_HashMap.py
+++ 706-Desigh_HashMap.py
@@ -3,69 +3,77 @@
 - O(n/k): n-number of all possible values, k-number of buckets, O(k+m): m - number of unique values
 - similar problem: 705-Design HashSet
 """
-class Node:
-    def __init__(self, key, val, next=None):
-        self.key = key
-        self.val = val
-        self.next = next
-    
-    def add(self, key, val):
-        prev = self
-        while prev.next:
-            cur = prev.next
-            if cur and cur.key == key:
-                cur.val = val
-                return
-            prev = cur
-        prev.next = Node(key, val)
-        
-    def delete(self, key):
-        prev = self
-        while prev.next:
-            cur = prev.next
-            if cur and cur.key == key:
-                prev.next = cur.next
-                return
-            prev = cur
-    
-    def find(self, key):
-        prev = self
-        while prev.next:
-            cur = prev.next
-            if cur and cur.key == key:
-                return cur.val
-            prev = cur
-        return -1
-
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.base = 1999
-        self.buckets = [Node(-1, -1) for _ in range(self.base)]
+        self.hash_factor = 1999
+        self.hashkey_to_ll = collections.defaultdict(LinkedList) # hashkey -> linkedlist
+        
+    def __hashfunc(self, key):
+        return key % self.hash_factor
         
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
-        index = key % self.base
-        self.buckets[index].add(key, value)
-        
+        hashkey = self.__hashfunc(key)
+        ll = self.hashkey_to_ll[hashkey]
+        node = ll.find(key)
+        if node:
+            node.val = value
+        else:
+            ll.add(key, value)        
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        index = key % self.base
-        return self.buckets[index].find(key)
-        
+        hashkey = self.__hashfunc(key)
+        ll = self.hashkey_to_ll[hashkey]
+        node = ll.find(key)
+        if not node:
+            return -1
+        return node.val   
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        index = key % self.base
-        self.buckets[index].delete(key)
+        hashkey = self.__hashfunc(key)
+        ll = self.hashkey_to_ll[hashkey]
+        ll.remove(key)
+        
+class LinkedNode:
+    def __init__(self, key=None, val=None, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+        
+class LinkedList:
+    def __init__(self, hashed_key=None):
+        self.hashkey = None
+        self.head = LinkedNode()
+    
+    def add(self, key, val):
+        node = LinkedNode(key, val)
+        node.next = self.head.next
+        self.head.next = node
+        
+    def remove(self, key):
+        prev, node = self.head, self.head.next
+        while node:
+            if node.key == key:
+                break
+            prev = node
+            node = node.next
+        if node:
+            prev.next = node.next
+            del node
+        
+    def find(self, key):
+        node = self.head
+        while node:
+            if node.key == key:
+                return node
+            node = node.next
+        return None
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
