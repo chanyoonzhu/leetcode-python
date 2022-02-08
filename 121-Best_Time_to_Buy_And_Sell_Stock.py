c911b776d8a16a6d64b47bfd26c526fff0d27ae6
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-07 20:50:31 -0800

    Updates 121-Best_Time_to_Buy_And_Sell_Stock

--- 121-Best_Time_to_Buy_And_Sell_Stock.py
+++ 121-Best_Time_to_Buy_And_Sell_Stock.py
@@ -1,18 +1,16 @@
+"""
+- https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+- Questions: buy first, then sell?
+- negative numbers? empty array? numbers getting smaller and smaller - Don't buy or profit can be negative?
+"""
+
+"""
+- Brute Force
+- O(n^2), O(1)
+- TLE
+"""
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        """
-        - Questions: buy first, then sell?
-        - negative numbers? empty array? numbers getting smaller and smaller - Don't buy?
-        
-        - Brute Force
-        - O(n^2), O(1)
-        - time limit exceeded
-        
         maxProfit = 0
         
         for i in range(len(prices)):
@@ -20,35 +18,12 @@ class Solution(object):
                 maxProfit = max(maxProfit, prices[j] - prices[i])
                 
         return maxProfit
-        """
         
-        """
-        - One pass 1: buy index increase until value smaller than sell, 
-
-                
-        maxProfit = 0
-        buy = sell = 0
-        
-        while sell < len(prices):
-            if prices[buy] <= prices[sell]:
-                maxProfit = max(maxProfit, prices[sell] - prices[buy])
-                sell += 1
-            else:
-                buy = sell
-            
-        return maxProfit
-        """
-
-        """
-        - One pass 2:
-        """
-
-        minPrice = float('inf')
-        maxProfit = 0
-        for price in prices:
-            if price < minPrice:
-                minPrice = price
-            elif price - minPrice > maxProfit:
-                maxProfit = price - minPrice
-        
-        return maxProfit
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lo = prices[0]
+        res = 0
+        for p in prices:
+            lo = min(lo, p)
+            res = max(res, p - lo)
+        return res
