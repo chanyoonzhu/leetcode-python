64ed2f508477fb57a84fe2b08820e8b9be5aad53
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-12 21:12:07 -0800

    Updates 1218-Longest_Arithmetic_Subsequence_of_Given_Difference

--- 1218-Longest_Arithmetic_Subsequence_of_Given_Difference.py
+++ 1218-Longest_Arithmetic_Subsequence_of_Given_Difference.py
@@ -4,7 +4,7 @@
 - O(n^2), O(n)
 """
 class Solution:
-    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+    def longestSubsequence(self, arr: list[int], difference: int) -> int:
         
         @lru_cache(None)
         def dp(i):
@@ -24,7 +24,7 @@ class Solution:
 - O(n), O(n)
 """
 class Solution:
-    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+    def longestSubsequence(self, arr: list[int], difference: int) -> int:
         
         dp = [1] * len(arr)
         number_last_pos = {}
@@ -42,10 +42,32 @@ class Solution:
 - O(n), O(n)
 """
 class Solution:
-    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+    def longestSubsequence(self, arr: list[int], difference: int) -> int:
         
         memo = {}
         for x in arr:
             memo[x] = memo[x-difference] + 1 if x-difference in memo else 1
             
         return max(memo.values())
+
+"""
+- follow-up: difference can be from 1 to difference (where difference > 0)
+
+"""
+class SolutionFollowUp:
+    def longestSubsequence(self, arr: list[int], difference: int) -> int:
+        
+        dp = [1] * len(arr)
+        number_last_pos = {}
+        res = 1
+        
+        for i, x in enumerate(arr):
+            for diff in range(1, difference + 1):
+                if x - diff in number_last_pos:
+                    dp[i] = max(dp[i], dp[number_last_pos[x - diff]] + 1)
+                    res = max(res, dp[i])
+            number_last_pos[x] = i
+        return res
+
+s = SolutionFollowUp()
+print(s.longestSubsequence([3, 1, 2, 5, 8, 13, 10], 3)) # 5 => [1， 2， 5， 8， 10]
