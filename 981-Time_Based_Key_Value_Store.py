07d17337a388e5fb1591aa7a24e3f5103ec6cb83
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-12 16:20:11 -0800

    Updates 981-Time_Based_Key_Value_Store

--- 981-Time_Based_Key_Value_Store.py
+++ 981-Time_Based_Key_Value_Store.py
@@ -38,7 +38,27 @@ class TimeMap:
         else:
             start = mid
         return self._binary_search(values, start, end, timestamp)
+
+"""
+- binary search with bisect
+"""    
+import bisect
+
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
         
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((timestamp, value))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        idx = bisect.bisect_right(self.time_map[key], (timestamp, "|")) # "|" comes after letters in ASCII table
+        if idx == 0: # easy to miss
+            return ""
+        return self.time_map[key][idx-1][1]
 
 
 # Your TimeMap object will be instantiated and called as such:
