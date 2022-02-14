ee4f196c004b3c34ab694c3f816d1505b0870763
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-13 17:16:29 -0800

    Revisits 207-Course_Schedule

--- 207-Course_Schedule.py
+++ 207-Course_Schedule.py
@@ -29,22 +29,25 @@ class Solution:
 """
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
+        taken = 0
         
-        indegrees = [0] * numCourses
         graph = defaultdict(set)
         for c, pre in prerequisites:
             graph[pre].add(c)
-            indegrees[c] += 1
-        
-        q = [c for c in range(numCourses) if not indegrees[c]]
+            indegree[c] += 1
+            
+        q = [c for c in range(numCourses) if indegree[c] == 0]
         while q:
             c = q.pop(0)
-            for nc in graph[c]:
-                indegrees[nc] -= 1
-                if indegrees[nc] == 0:
-                    q.append(nc)
+            taken += 1
+            for nei_c in graph[c]:
+                if indegree[nei_c]: # skip if already taken
+                    indegree[nei_c] -= 1
+                    if indegree[nei_c] == 0:
+                        q.append(nei_c)
         
-        return not any(indegrees)
+        return taken == numCourses
 
 sl = Solution()
 print(sl.canFinish(3, [[1,0],[1,2],[0,1]]))
