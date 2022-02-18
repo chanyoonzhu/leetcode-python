13b6b7d7011f360f73b4cb0ae1cbaf922dc3ac26
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-17 23:28:44 -0800

    Revisits 1014-Best Sightseeing Pair

--- 1014-Best_Sightseeing_Pair.py
+++ 1014-Best_Sightseeing_Pair.py
@@ -1,11 +1,12 @@
 """
 - Array
 - O(n), O(1)
+- similar: 121-Best Time to Buy and Sell Stock (+ dist penalty); 1937-Maximum Number of Points with Cost
 """
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        cur = res = 0 # cur - cur max (with decrements considered)
+        value_i_with_dist_penalty = res = 0 # value_i_with_dist_penalty - max value[k] + i - j of (k from 0...i)
         for value in values:
-            res = max(res, cur + value)
-            cur = max(cur, value) - 1
+            res = max(res, value + value_i_with_dist_penalty)
+            value_i_with_dist_penalty = max(value_i_with_dist_penalty, value) - 1 # each move add dist penalty 1
         return res
