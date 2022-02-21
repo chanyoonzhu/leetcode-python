71b122d6b3b1072819b350f81ac0ba4588c5633a
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-20 22:47:52 -0800

    Updates 772-Basic_Calculator_III

--- 772-Basic_Calculator_III.py
+++ 772-Basic_Calculator_III.py
@@ -45,3 +45,63 @@ class Solution:
             return (result + prev, i)
         
         return helper(s + '+')[0]
+
+"""
+- two stacks
+"""
+class Solution:
+    def calculate(self, s: str) -> int:
+        funcs = {
+            "+": lambda a,b: a+b,
+            "-": lambda a,b: a-b,
+            "*": lambda a,b: a*b,
+            "/": lambda a,b: a//b if (a >= 0) == (b >= 0) else -(abs(a) // abs(b)),
+        }
+        precedences = {
+            "(": -1,
+            "+": 0,
+            "-": 0,
+            "*": 1,
+            "/": 1
+        }
+        
+        def operate():
+            print(operands, operators)
+            c = operators.pop()
+            a = operands.pop()
+            b = operands.pop()
+            return funcs[c](b,a)
+        
+        def compare(op1, op2):
+            return precedences[op1] - precedences[op2]
+        
+        operands = []
+        operators = []
+        
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                val = int(c)
+                while i + 1 < len(s) and s[i+1].isdigit():
+                    val = 10 * val + int(s[i+1])
+                    i += 1
+                operands.append(val)
+            elif c == " ":
+                continue
+            elif c == "(":
+                operators.append(c)
+            elif c == ")":
+                while (operators[-1] != '('):
+                    operands.append(operate())
+                operators.pop() # easy to miss: pop "("
+            else:
+                while operators and compare(c, operators[-1]) <= 0: # calculate if prev operator has higher precedence
+                    operands.append(operate())
+                operators.append(c)
+            i += 1
+        
+        while operators:
+            operands.append(operate())
+        
+        return operands[-1]
