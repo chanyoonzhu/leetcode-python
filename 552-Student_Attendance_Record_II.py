64feb9d7189495d8ffe7dc0640e940badea150c7
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-20 17:02:31 -0800

    Revisits 552-Student_Attendance_Record_II

--- 552-Student_Attendance_Record_II.py
+++ 552-Student_Attendance_Record_II.py
@@ -11,11 +11,11 @@ class Solution:
         # i=0, possible = 1; i=1, possible = 2 (P,L); i=2, possible = 4 (PP,PL,LP,LL)
         dp[0], dp[1], dp[2] = 1, 2, 4
         for i in range(3,n + 1):
-            # end with "P": dp[i-1]; end with "PL": dp[i-2]; end with "PLL": dp[i-3]; end with "LLL": is not allowed
+            # consider ending with distinct eligible suffixes: end with "P": dp[i-1]; end with "PL": dp[i-2]; end with "PLL": dp[i-3]; end with "LLL": is not allowed
             dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3] ) % mod
         res = dp[n]
         # with one 'A'
-        for i in range(1, n + 1):
+        for i in range(1, n + 1): # insert 'A' at i
             res += dp[i - 1] * dp[n - i] % mod
         res %= mod
         return res
