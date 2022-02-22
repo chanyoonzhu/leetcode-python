2a6d6fe819e244848cfeb37f6124e2ae5dd7201b
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-21 21:07:43 -0800

    Revisits 1723-Find_Minimum_Time_to_Finish_All_Jobs

--- 1723-Find_Minimum_Time_to_Finish_All_Jobs.py
+++ 1723-Find_Minimum_Time_to_Finish_All_Jobs.py
@@ -1,28 +1,57 @@
 """
 - binary search + backtracking
+- TLE
 """
 class Solution:
-    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
-        jobs.sort(reverse=True) # assign jobs with longer time first
-        lo, hi = max(jobs), sum(jobs)
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:       
         
-        # backtracking
-        def canFinish(idx):
-            if idx == len(jobs): return True
-            for i in range(len(cap)): # assign job at idx to each worker, try which one works
-                if cap[i] >= jobs[idx]:
-                    cap[i] -= jobs[idx] 
-                    if canFinish(idx + 1):
+        def canComplete(i, available_times):
+            if i == len(jobs):
+                return True
+            for j in range(len(available_times)):
+                if available_times[j] >= jobs[i]:
+                    available_times[j] -= jobs[i]
+                    if canComplete(i + 1, available_times):
                         return True
-                    cap[i] += jobs[idx]
-                if cap[i] == mid: # pruning otherwise TLE: cap[i] cannot be assigned anything to make this work, exit
-                    break
+                    available_times[j] += jobs[i]
             return False
+            
         
+        lo, hi = max(jobs), sum(jobs)
         while lo < hi:
             mid = lo + (hi - lo) // 2
-            cap = [mid] * k
-            if canFinish(0):
+            
+            if canComplete(0, [mid] * k):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+
+"""
+- binary search + backtracking
+- with pruning
+"""
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:       
+        
+        def canComplete(i, available_times, max_time):
+            if i == len(jobs):
+                return True
+            for j in range(len(available_times)):
+                if available_times[j] >= jobs[i]:
+                    available_times[j] -= jobs[i]
+                    if canComplete(i + 1, available_times, max_time):
+                        return True
+                    available_times[j] += jobs[i]
+                if available_times[j] == max_time: # pruning: cap[i] cannot be assigned any job to make this work, exit
+                    return False
+            return False
+            
+        jobs.sort(reverse=True) # assign large job first
+        lo, hi = max(jobs), sum(jobs)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2  
+            if canComplete(0, [mid] * k, mid):
                 hi = mid
             else:
                 lo = mid + 1
