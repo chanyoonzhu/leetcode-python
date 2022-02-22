195236ed315579a0f0e6b1553eee81b9531917da
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-21 22:33:10 -0800

    Updates 1834-Single-Threaded_CPU

--- 1834-Single-Threaded_CPU.py
+++ 1834-Single-Threaded_CPU.py
@@ -4,20 +4,21 @@
 """
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = [(e, p, i) for i, (e, p) in enumerate(tasks)]
-        tasks.sort()
-        time = task_i = 0
-        h = []
-        result = []
-        while task_i < len(tasks) or h:
-            while task_i < len(tasks) and tasks[task_i][0] <= time:
-                e, p, i = tasks[task_i]
-                heapq.heappush(h, (p, i))
-                task_i += 1
+        
+        N = len(tasks)
+        tasks_sorted = sorted([(tasks[i][0], tasks[i][1], i) for i in range(N)], key=lambda x: x[0])
+        res = []
+        h = [] # processing time, idx
+        
+        i = tasks_sorted[0][0]
+        while len(res) < len(tasks):
+            while tasks_sorted and tasks_sorted[0][0] <= i:
+                _, processing_time, idx = tasks_sorted.pop(0)
+                heapq.heappush(h, (processing_time, idx))
             if h:
-                p, i = heapq.heappop(h)
-                result.append(i)
-                time += p
-            else:
-                time = tasks[task_i][0]     
-        return result
+                processing_time, idx = heapq.heappop(h)
+                res.append(idx)
+                i += processing_time
+            else: # easy to miss, no start time is smaller than i, need to go with the next start time
+                i = tasks_sorted[0][0] 
+        return res
