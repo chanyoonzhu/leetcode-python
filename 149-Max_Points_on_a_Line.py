7a10011ed3cc280e0fb4e141781defeb07870bee
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-21 22:15:03 -0800

    Updates 149-Max_Points_on_a_Line

--- 149-Max_Points_on_a_Line.py
+++ 149-Max_Points_on_a_Line.py
@@ -36,18 +36,38 @@ class Solution:
         
         return result
 
-        """ can use math.gcd
-        def gcd(n1, n2):
-            if n1 < 0:
-                n1 = -n1
-            if n2 < 0:
-                n2 = -n2
-            while n1 != n2:
-                if n1 > n2:
-                    n1 -= n2
-                else:
-                    n2 -= n1
-            return n1
-        """
+"""
+- simpler
+- one counter for each point 1: no need to consider c, only slope is needed
+"""
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        def gcd(m, n):
+            if not n:
+                return m 
+            return gcd(n, m % n)
+                
+        def getslope(p1, p2):
+            dx = p1[0] - p2[0]
+            dy = p1[1] - p2[1]
+            
+            # easy to miss: parallel to axis
+            if dx == 0: return (1, 0)
+            if dy == 0: return (0, 1)
+            
+            d = gcd(dx, dy)
+            return (dx // d, dy // d)
+        
+        res = 0
+        for i in range(len(points)):
+            slope_counts = Counter()
+            p1 = points[i]
+            for j in range(i+1, len(points)):
+                p2 = points[j]
+                slope = getslope(p1, p2)
+                slope_counts[slope] += 1
+                res = max(res, slope_counts[slope])
+            
+        return res + 1
             
         
