e8e64caf1fe1bd303bd332d6056751f250b14ee3
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-22 21:49:18 -0800

    Updates 2034-Stock_Price_Fluctuation

--- 2034-Stock_Price_Fluctuation.py
+++ 2034-Stock_Price_Fluctuation.py
@@ -39,6 +39,61 @@ class StockPrice:
     def minimum(self) -> int:
         return self._price_to_count.peekitem(0)[0]
 
+"""
+- follow up: add drop
+"""
+from sortedcontainers import SortedDict
+
+class StockPrice:
+
+    def __init__(self):
+        self.sorted_price_to_count = SortedDict()
+        self.timestamp_to_price = dict()
+        self.latest_timestamp = 0
+        
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp > self.latest_timestamp:
+            self.latest_timestamp = timestamp
+        if timestamp in self.timestamp_to_price:
+            prev_price = self.timestamp_to_price[timestamp]
+            self.sorted_price_to_count[prev_price] -= 1
+            if self.sorted_price_to_count[prev_price] == 0:
+                del self.sorted_price_to_count[prev_price]
+        self.timestamp_to_price[timestamp] = price
+        self.sorted_price_to_count.setdefault(price, 0)
+        self.sorted_price_to_count[price] += 1 
+        
+    def drop(self, timestamp: int) -> None:
+        if timestamp in self.timestamp_to_price:
+            price = self.timestamp_to_price[timestamp]
+            del self.timestamp_to_price[timestamp]
+            self.sorted_price_to_count[price] -= 1
+            if self.sorted_price_to_count[price] == 0:
+                del self.sorted_price_to_count[price]
+            if timestamp == self.latest_timestamp:
+                self.latest_timestamp = max(self.timestamp_to_price.keys())
+                
+
+    def current(self) -> int:
+        return self.timestamp_to_price[self.latest_timestamp]
+        
+
+    def maximum(self) -> int:
+        return self.sorted_price_to_count.peekitem(-1)[0]
+        
+
+    def minimum(self) -> int:
+        return self.sorted_price_to_count.peekitem(0)[0]
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
+
 
 # Your StockPrice object will be instantiated and called as such:
 # obj = StockPrice()
