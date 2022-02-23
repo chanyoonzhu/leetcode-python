191ed80dee7e1fe71937d49aa44579a1479c0413
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-22 21:17:24 -0800

    Adds Google-Bank_Wait_Time

---
+++ google/Google-Bank_Wait_Time.py
@@ -0,0 +1,69 @@
+"""
+- n个服务员，m个人等位，给了数组里面元素是每个服务员服务一个人的时间，m>>n，当多个服务员空闲时候，编号最小的去服务，问当你是第m+1个人的时候要等多久
+"""
+import heapq
+from collections import Counter
+
+"""
+- O(m)
+"""
+class Solution:
+    def calcWaitTime(self, efficiency: list, customers: int) -> int:
+        h = [(time, i) for i, time in enumerate(efficiency)] # end_time, index
+        heapq.heapify(h)
+        served, cur_time = len(efficiency), 0
+        while served < customers:
+            end_time, idx = heapq.heappop(h)
+            heapq.heappush(h, (end_time + efficiency[idx], idx))
+            cur_time = end_time
+            served += 1
+        return cur_time
+
+import heapq
+
+"""
+- O(lcm(efficiency))
+"""
+class Solution2:
+    def calcWaitTime(self, efficiency: list, customers: int) -> int:
+
+        lcm = self.getLcm(efficiency)
+        # at time = lcm, completes serving batch_customers
+        batch_customers = sum([lcm // time for time in efficiency])
+        served = cur_time = 0
+        if customers > batch_customers:
+            batches = customers // batch_customers
+            cur_time += lcm * batches
+            served = batch_customers * batches
+
+        h = [(cur_time + time, i) for i, time in enumerate(efficiency)] # end_time, index
+        served += len(efficiency)
+        heapq.heapify(h)
+        while served < customers:
+            end_time, idx = heapq.heappop(h)
+            heapq.heappush(h, (end_time + efficiency[idx], idx))
+            cur_time = end_time
+            served += 1
+        return cur_time
+
+    def getLcm(self, nums):
+        h = [(n, n) for n in nums]
+        heapq.heapify(h)
+        sieves = Counter()
+        while h:
+            slot, n = heapq.heappop(h)
+            sieves[slot] += 1
+            if sieves[slot] == len(nums):
+                print(f"lcm = {slot}")
+                return slot
+            heapq.heappush(h, (slot + n, n))
+
+efficiency = [6,7,3,4,1,5]
+customers = 100000000
+s1 = Solution()
+s2 = Solution2()
+# print(s1.calcWaitTime(efficiency, customers))
+print(s2.calcWaitTime(efficiency, customers))
+
+
+            
