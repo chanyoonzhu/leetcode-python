cde2f42a6ad197c994c12d2632d9177c5e216e02
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-26 17:35:11 -0800

    Updates 2128-Remove_All_Ones_With_Row_and_Column_Flips

--- 2128-Remove_All_Ones_With_Row_and_Column_Flips.py
+++ 2128-Remove_All_Ones_With_Row_and_Column_Flips.py
@@ -1,6 +1,11 @@
 """
 - math
-- https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips/discuss/1671908/Python-3-or-Math-or-Explanation
+- intuition: there are only two patterns each row(or col) could have: the pattern of the first row, or the reversed-pattern of the first row
+- proof: 
+    1. supposed a group of flips can bring all the elements to 0, for each cell, the total number of flips will stay static no matter how we change the sequences of the flips, 
+    so the end status of each cell should stay the same disregard the sequence of flips
+    2. lets do all the row flips first and then the col flips, for col flips to be able to get all 0s, each col needs to be either all 0 or all 1
+    3. for each col to be either all 0 or all 1, each row must be of only two patterns that are exclusive of each other (anti-pattern)
 """
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
