668fc26ca2aad697dab30c58e6d1505d2c5d08e2
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-26 16:09:44 -0800

    Revisits 1146-Snapshot_Array

--- 1146-Snapshot_Array.py
+++ 1146-Snapshot_Array.py
@@ -36,6 +36,7 @@ class SnapshotArray:
         snap(): O(1), O(1)
         get(int index, int snap_id): O(log(Set)), O(1)
     """
+class SnapshotArray:
     def __init__(self, length: int):
         self.history = [[[0, 0]] for _ in range(length)]
         self.snapshot_id = -1
@@ -74,6 +75,7 @@ class SnapshotArray:
         snap(): O(1), O(1)
         get(int index, int snap_id): O(log(Set)), O(1)
     """
+class SnapshotArray:
     def __init__(self, length: int):
         self.history = [[[-1, 0]] for _ in range(length)]
         self.snapshot_id = -1
@@ -99,30 +101,36 @@ class SnapshotArray:
                 j = mid - 1
         return history[i][1]
     
-    """
-    - array and binary search with bisect
-    - java can use Treemap
-    -   SnapshotArray(int length): O(n), O(n)
-        set(int index, int val): O(1), O(1)
-        snap(): O(1), O(1)
-        get(int index, int snap_id): O(log(Set)), O(1)
-    """
+"""
+- array and binary search with bisect
+- java can use Treemap
+-   SnapshotArray(int length): O(n), O(n)
+    set(int index, int val): O(1), O(1)
+    snap(): O(1), O(1)
+    get(int index, int snap_id): O(log(Set)), O(1)
+"""
+class SnapshotArray:
+
     def __init__(self, length: int):
-        self.history = [[[-1, 0]] for _ in range(length)]
-        self.snapshot_id = -1
+        self.snap_id = -1
+        self.num_snaps = [[(-1, 0)] for _ in range(length)] # list of (snap_id, value)
         
+
     def set(self, index: int, val: int) -> None:
-        self.history[index].append([self.snapshot_id + 1, val])
-        return
+        prev_snap_id, prev_val = self.num_snaps[index][-1]
+        if prev_snap_id == self.snap_id:
+            self.num_snaps[index][-1] = (self.snap_id, val)
+        else:
+            self.num_snaps[index].append((self.snap_id, val))      
 
     def snap(self) -> int:
-        self.snapshot_id += 1
-        return self.snapshot_id
-
+        self.snap_id += 1
+        return self.snap_id
+        
     def get(self, index: int, snap_id: int) -> int:
-        history = self.history[index]  
-        curr_snap_pos = bisect.bisect(history, [snap_id + 1]) - 1 # have to make snap_id + 1 a list to be searchable with bisect
-        return history[curr_snap_pos][1]
+        snaps = self.num_snaps[index] # snap_id, val
+        idx = bisect.bisect_left(snaps, (snap_id, -1)) - 1 # have to make snap_id + 1 a list to be searchable with bisect
+        return snaps[idx][1]
                 
                                       
         
