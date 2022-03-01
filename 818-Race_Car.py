95ef0c8c01b4fa94dacf0d298998d69134c30350
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-02-28 20:48:10 -0800

    Updates 818-Race_Car

--- 818-Race_Car.py
+++ 818-Race_Car.py
@@ -20,23 +20,27 @@ class Solution:
 """
 class Solution:
     def racecar(self, target: int) -> int:
-        return self.dp(target)
-        
+        return self.helper(target)
+    
     @lru_cache(None)
-    def dp(self, target):
-        steps_forward = target.bit_length() # observation: steps_forward - 1: closest before arriving target; steps_forward - right at or just exceeding target
-        instructions = float("inf")
-        # case 1: all the way to target using A
-        if 2 ** steps_forward - 1 == target:
-            instructions = min(instructions, steps_forward)
-        else:
-            # case 2: A util pass target (stop immediately when past), turn, then A again
-            forward_dist = 2 ** steps_forward - 1
-            instructions = min(instructions, self.dp(forward_dist - target) + steps_forward + 1) # 1 - for the turn
-            # case 3: A as far as possible (but not exceeding target), turn, turn, A to target
-            steps_forward -= 1
-            forward_dist = 2 ** steps_forward - 1
-            for steps_backward in range(steps_forward): 
-                backward_dist = 2 ** steps_backward - 1
-                instructions = min(instructions, self.racecar(target - (forward_dist - backward_dist)) + steps_forward + 1 + steps_backward + 1) # +1 twice for the two turns
-        return instructions
+    def helper(self, dist):
+        if dist == 0:
+            return 0
+        steps_forward = floor(log(dist + 1, 2)) # easy to miss: +1 (dist is 0 based)
+        # case 1. forward until reach
+        forward_dist = 2 ** (steps_forward) - 1
+        if 2 ** (steps_forward) - 1 == dist:
+            return steps_forward
+
+        steps = float("inf")
+        # case 2. forward, turn, back, turn, forward
+        for steps_backward in range(steps_forward):
+            backward_dist = 2 ** steps_backward - 1
+            steps = min(steps, steps_forward + steps_backward + self.helper(dist - forward_dist + backward_dist) + 2)
+        # case 3. forward (exceed), turn, forward
+        steps_forward += 1
+        forward_dist = 2 ** (steps_forward) - 1
+        steps = min(steps, steps_forward + 1 + self.helper(forward_dist - dist))
+        return steps
+            
+        
