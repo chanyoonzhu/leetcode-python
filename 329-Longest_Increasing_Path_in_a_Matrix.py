ced934c014bc0dd9fd127930d433688b9b595fcb
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-01 20:54:09 -0800

    Adds 329-Longest_Increasing_Path_in_a_Matrix

--- 329-Longest_Increasing_Path_in_a_Matrix.py
+++ 329-Longest_Increasing_Path_in_a_Matrix.py
@@ -4,31 +4,27 @@
 - O(mn), O(mn)
 """
 class Solution:
-    
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        self.matrix = matrix
+        self.dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        self.m = len(matrix)
+        self.n = len(matrix[0])
+        self.res = 0
+        for r in range(self.m):
+            for c in range(self.n):
+                self.dfs(r, c)
+        return self.res
         
-        def dfs(x, y, longest):
-            for i, j in move:
-                x_next = x + i
-                y_next = y + j
-                if 0 <= x_next < n and 0 <= y_next < m and matrix[x_next][y_next] > matrix[x][y]:
-                    next_longest_path = longest_paths[x_next][y_next] if (x_next, y_next) in visited else dfs(x_next, y_next, 1)
-                    longest = max(longest, next_longest_path + 1)
-            visited.add((x, y))
-            longest_paths[x][y] = longest
-            return longest
-            
-        
-        n, m = len(matrix), len(matrix[0])
-        longest_paths = [[1] * m for _ in range(n)]
-        visited = set()
-        move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        result = 0
-        
-        for i in range(n):
-            for j in range(m):
-                result = max(result, dfs(i, j, 1))
-        return result
+    @lru_cache(None)
+    def dfs(self, r, c):
+        connected = 0
+        for i, j in self.dir:
+            nr, nc = r + i, c + j
+            if 0 <= nr < self.m and 0 <= nc < self.n and self.matrix[nr][nc] > self.matrix[r][c]:
+                connected = max(connected, self.dfs(nr, nc))
+        connected += 1 # itself
+        self.res = max(self.res, connected)
+        return connected
     
 """
 - topological sorting
