e106f820ebc9a273217d3964a10e168fecfc6922
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-07 22:18:43 -0800

    Updates 678-Valid_Parenthesis_String

--- 678-Valid_Parenthesis_String.py
+++ 678-Valid_Parenthesis_String.py
@@ -17,3 +17,24 @@ class Solution:
                 opens_max -= 1
             if opens_max < 0: return False
         return opens_min == 0
+
+"""
+- dynamic programming
+"""
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        N = len(s)
+        
+        @lru_cache(None)
+        def dp(i, open_count):
+            if i == N:
+                return open_count == 0
+            if s[i] == "(":
+                return dp(i + 1, open_count + 1)
+            elif s[i] == ")":
+                if open_count < 1: return False
+                return dp(i + 1, open_count - 1)
+            else:
+                return dp(i + 1, open_count + 1) or dp(i + 1, open_count) or (open_count >= 1 and dp(i + 1, open_count - 1))
+            
+        return dp(0, 0)
