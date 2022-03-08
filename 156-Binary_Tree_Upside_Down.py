895ed36248e66e7c006341f47487dcad1a7b6a4e
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-07 21:36:46 -0800

    Updates 156-Binary_Tree_Upside_Down

--- 156-Binary_Tree_Upside_Down.py
+++ 156-Binary_Tree_Upside_Down.py
@@ -6,6 +6,11 @@
 #         self.right = right
 
 """
+- Q: does every right node have a sibling?
+- Q: how to handle children of right node? (no children on right node)
+"""
+
+"""
 - dfs (recursive) - my solution
 - O(n), O(n)
 """
@@ -41,3 +46,24 @@ class Solution:
             return res
         else:
             return root
+
+"""
+- dfs (iterative)
+- O(n), O(n)
+"""
+class Solution:
+    def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        stack = []
+        node = root
+        while node:
+            stack.append(node)
+            node = node.left
+        
+        new_root = stack[-1]
+        while len(stack) > 1:
+            node = stack.pop()
+            node.left = stack[-1].right
+            node.right = stack[-1]
+            stack[-1].left, stack[-1].right = None, None
+        return new_root
