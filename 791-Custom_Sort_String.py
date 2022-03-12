a95c3ec64d6c3ff1b77e2ac023ba068ddff3ac3c
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-12 11:54:02 -0800

    Updates 791-Custom_Sort_String

--- 791-Custom_Sort_String.py
+++ 791-Custom_Sort_String.py
@@ -1,18 +1,15 @@
 """
 - hashmap
-- O(len(s))
+- O(len(s)), O(1)
 """
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        counter = Counter(s)
-        res = ""
-        
-        for c in order: # O(26) -> O(1)
-            if counter[c]:
-                res += c * counter[c]
-                del counter[c]
-        
-        for c in counter: #O(len(s))
-            res += c * counter[c]
-        
-        return res
+        counts = Counter(s)
+        res = []
+        for c in order:
+            if c in counts:
+                res.extend([c] * counts[c])
+                del counts[c]
+        for c in counts: # easy to miss: append those that are not in "order"
+            res.extend([c] * counts[c])
+        return "".join(res)
