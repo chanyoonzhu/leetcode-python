8ee7b595ddeaa1954dc56e116a4839ad01175eff
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-11 21:51:07 -0800

    Updates 543-Diameter_of_Binary_Tree

--- 543-Diameter_of_Binary_Tree.py
+++ 543-Diameter_of_Binary_Tree.py
@@ -25,6 +25,19 @@ class Solution:
         dfs(root)
         return self.result - 1 # diameter = node number - 1
 
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        return(self.dfs(root)[1] - 1)
+        
+    def dfs(self, node):
+        if not node:
+            return (0, 0) # max_half_length, max_full_length
+        l_half, l_full = self.dfs(node.left)
+        r_half, r_full = self.dfs(node.right)
+        full = l_half + r_half + 1
+        return (max(l_half, r_half) + 1, max(l_full, r_full, full))
+
 n1 = TreeNode(1)
 n1.left = TreeNode(2)
 n1.right = TreeNode(3)
