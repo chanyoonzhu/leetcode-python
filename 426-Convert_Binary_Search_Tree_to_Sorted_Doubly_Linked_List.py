fda7b087a73b6dbc1888a2b2316b6ba4393876af
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-12 11:28:04 -0800

    Revisits 426-Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List

--- 426-Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.py
+++ 426-Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.py
@@ -15,24 +15,23 @@ class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
         
         def dfs(node):
-            if node.left:
-                left_leftmost, left_rightmost = dfs(node.left)
-                left_rightmost.right = node
-                node.left = left_rightmost
-            else:
-                left_leftmost = node
-            if node.right:
-                right_leftmost, right_rightmost = dfs(node.right)
-                right_leftmost.left = node
-                node.right = right_leftmost
-            else:
-                right_rightmost = node
-            return (left_leftmost, right_rightmost)
-        
-        if not root: return None
+            if not node:
+                return (None, None) # leftmost, rightmost
+            l_leftmost, l_rightmost = dfs(node.left)
+            r_leftmost, r_rightmost = dfs(node.right)
+            if l_rightmost:
+                l_rightmost.right = node
+                node.left = l_rightmost
+            if r_leftmost:
+                r_leftmost.left = node
+                node.right = r_leftmost
+            return (l_leftmost or node, r_rightmost or node)
+    
+        if not root:
+            return None
         leftmost, rightmost = dfs(root)
-        leftmost.left = rightmost
         rightmost.right = leftmost
+        leftmost.left = rightmost
         return leftmost
 
 """
@@ -70,17 +69,21 @@ class Solution:
 - dfs (in-order traversal): iterative
 - O(n), O(n)
 """
-def treeToDoublyList(self, root):
-    if not root: return
-    dummy = Node(0, None, None)
-    prev = dummy
-    stack, node = [], root
-    while stack or node:
-        while node:
-            stack.append(node)
-            node = node.left
-        node = stack.pop()
-        node.left, prev.right, prev = prev, node, node
-        node = node.right
-    dummy.right.left, prev.right = prev, dummy.right
-    return dummy.right
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+    
+        if not root:
+            return None
+        stack = []
+        node = root
+        start = prev = Node(-1)
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            prev.right, node.left, prev = node, prev, node
+            node = node.right
+            
+        start.right.left, prev.right = prev, start.right
+        return start.right
