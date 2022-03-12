370982f9702a535870f99825cb6ee3261dbdfa6b
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-11 23:24:20 -0800

    Updates 314-Binary_Tree_Vertical_Order_Traversal

--- 314-Binary_Tree_Vertical_Order_Traversal.py
+++ 314-Binary_Tree_Vertical_Order_Traversal.py
@@ -7,24 +7,6 @@
 
 """
 - dfs
-- wrong answer: node on the left tree always printed first (higher level should be printed first)
-"""
-class Solution:
-    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        level_to_val = defaultdict(list)
-        self.dfs(root, 0, level_to_val)
-        return [level_to_val[level] for level in sorted(level_to_val)]
-        
-        
-    def dfs(self, node, level, level_to_val):
-        if not node: return
-        level_to_val[level].append(node.val)
-        self.dfs(node.left, level-1, level_to_val)
-        self.dfs(node.right, level+1, level_to_val)
-
-
-"""
-- dfs
 - add height for sorting
 """
 class Solution:
@@ -50,14 +32,16 @@ class Solution:
 """
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
         level_to_vals = defaultdict(list)
-        bfs = deque()
-        if root: bfs.append((0, root)) # level, node
+        q = deque()
+        if root: q.append((root, 0)) # node, level
         
-        while bfs:
-            level, node = bfs.popleft()
+        while q:
+            node, level = q.popleft()
             level_to_vals[level].append(node.val)
-            if node.left: bfs.append((level - 1, node.left))
-            if node.right: bfs.append((level + 1, node.right))
+            if node.left: q.append((node.left, level - 1))
+            if node.right: q.append((node.right, level + 1))
         
-        return [level_to_vals[level] for level in sorted(level_to_vals)]  
+        return [level_to_vals[level] for level in sorted(level_to_vals)]
+        
