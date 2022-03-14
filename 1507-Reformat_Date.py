15cba0ac7f39bf07e1cae3ceab61acbe2b258744
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-13 17:44:12 -0700

    Adds 1507-Reformat_Date

---
+++ 1507-Reformat_Date.py
@@ -0,0 +1,14 @@
+"""
+- hashmap
+- O(n), O(1)
+"""
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        month_map = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May" : "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12", }
+        
+        converted = date[-4:] + "-" + month_map[date[-8:-5]] + "-"
+        if (len(date) == 13):
+            converted += date[:2]
+        else:
+            converted += "0" + date[0]
+        return converted
