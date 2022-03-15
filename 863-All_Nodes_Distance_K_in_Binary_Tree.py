a80f63b2c817c5b73f530a5f0ba03297327cfb5c
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-14 23:44:12 -0700

    Updates 863-All_Nodes_Distance_K_in_Binary_Tree

--- 863-All_Nodes_Distance_K_in_Binary_Tree.py
+++ 863-All_Nodes_Distance_K_in_Binary_Tree.py
@@ -51,6 +51,56 @@ class Solution(object):
         dfs(root)
         return self.result
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""
+- dfs - two dfs, one for traverse child, one for provide info for parent
+- O(n), O(n)
+"""
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        
+        root_to_target = [] # 0 - left, 1 - right
+        res = []
+        
+        def findParentPath(node, path):
+            if not node:
+                return None
+            if node.val == target.val:
+                return path
+            l = findParentPath(node.left, path + [0])
+            r = findParentPath(node.right, path + [1])
+            return l or r
+            
+        def dfs(node, k):
+            if not node:
+                return
+            if k == 0:
+                res.append(node.val)
+            dfs(node.left, k-1)
+            dfs(node.right, k-1)
+            
+        root_to_target = findParentPath(root, [])
+        dfs(target, k) # find decendents with distance k
+        # find nodes with distance k that are not decendents
+        parent_node = root
+        for i, direction in enumerate(root_to_target):
+            dist = len(root_to_target) - i # dist to target
+            if dist < k:
+                if direction == 0:
+                    dfs(parent_node.right, k - dist - 1)
+                else:
+                    dfs(parent_node.left, k - dist - 1)
+            elif dist == k:
+                res.append(parent_node.val)
+            parent_node = parent_node.left if direction == 0 else parent_node.right
+        return res
+
 """
 - graph + bfs : todo
 - O(n), O(n)
