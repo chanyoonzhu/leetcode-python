89d24f310fa618a1d5007ac53ecef83149fd9086
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-19 16:37:33 -0700

    Updates 698-Partition to K Equal Sum Subsets

--- 698-Partition_to_K_Equal_Sum_Subsets.py
+++ 698-Partition_to_K_Equal_Sum_Subsets.py
@@ -1,6 +1,7 @@
 """
 - backtracking
 - O(k * 2^n)
+- TlE
 """
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
@@ -19,7 +20,30 @@ class Solution:
                 if self.backtrack(parts, nums, i + 1):
                     return True
                 parts[j] += nums[i]
+        return False
 
 """
 - todo: bit-masking
 """
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        sub_sum, mod = divmod(sum(nums), k)
+        if mod != 0: return False
+        nums.sort(reverse=True)
+        n = len(nums)
+
+        bitmask = (1 << n) - 1
+    
+        @lru_cache(None)
+        def backtrack(part, bitmask):
+            if not bitmask:
+                return part == 0
+            if part == 0:
+                return backtrack(sub_sum, bitmask)
+            res = False
+            for i in range(n):
+                if bitmask & (1 << i) and nums[i] <= part:
+                    if backtrack(part - nums[i], bitmask ^ (1 << i)):
+                        return True
+        
+        return backtrack(sub_sum, bitmask)
