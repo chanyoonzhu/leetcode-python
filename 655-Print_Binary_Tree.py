e691b1858880df03a32a71948944422a0b404375
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-19 18:06:32 -0700

    Updates 655-Print_Binary_Tree

--- 655-Print_Binary_Tree.py
+++ 655-Print_Binary_Tree.py
@@ -9,23 +9,28 @@
 - dfs
 - O(n), O(n)
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
         M = self.getHeight(root)
         N = 2 ** M - 1
         res = [[""] * N for _ in range(M)]
-        self.dfs(root, 0, N // 2, res)
+        self.dfs(root, 0, 0, N-1, res)
         return res
         
-    # an easier-to-understand solution: https://leetcode.com/problems/print-binary-tree/discuss/106240/Python-recursive-solution-easy-to-understand
-    def dfs(self, node, row, col, printed):
+    def dfs(self, node, row, col_start, col_end, printed):
         if not node:
             return
+        col = (col_start + col_end) // 2
         printed[row][col] = str(node.val)
         row += 1
-        dist_to_parent = 2 ** (len(printed) - row - 1)
-        self.dfs(node.left, row, col - dist_to_parent, printed)
-        self.dfs(node.right, row, col + dist_to_parent, printed)
+        self.dfs(node.left, row, col_start, col - 1, printed)
+        self.dfs(node.right, row, col + 1, col_end, printed)
         
     
     # 1-based height
