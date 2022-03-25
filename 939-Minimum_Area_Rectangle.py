f77315a4df5bf5426507d9cff80321e8b2aef3bc
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-24 23:24:29 -0700

    Revisits 939-Minimum_Area_Rectangle

--- 939-Minimum_Area_Rectangle.py
+++ 939-Minimum_Area_Rectangle.py
@@ -12,7 +12,7 @@ class Solution(object):
         for i in range(size):
             for j in range(i+1, size):
                 if (points[i][0] != points[j][0] and points[i][1] != points[j][1]
-                    and (points[i][0], points[j][1]) in pointsSet and (points[j][0], points[i][1]) in pointsSet):
+                    and (points[i][0], points[j][1]) in pointsSet and (points[j][0], points[i][1]) in pointsSet): # check if two points form the diagnal of a rectangle
                     minArea = min(minArea, abs(points[i][1] - points[j][1]) * abs(points[i][0] - points[j][0]))
                     
         return minArea if minArea != float('inf') else 0
