ef0eee890573f2d2cf4d28953a2ded63cf49de3d
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-26 16:42:37 -0700

    Revisits 031-Next_Permutation

--- 031-Next_Permutation.py
+++ 031-Next_Permutation.py
@@ -1,5 +1,6 @@
 """
 - Array
+- intuition: replace the number A with the number B which is just larger than itself among the numbers lying to its right section
 - O(n), O(1)
 """
 class Solution:
@@ -7,25 +8,37 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # replace the number A with the number B which is just larger than itself among the numbers lying to its right section
-        # find the number(A) before the longest non-increasing suffix
-        right = len(nums)-1
-        while right > 0 and nums[right-1] >= nums[right]:
-            right -= 1
-        if right == 0:
-            return self.reverse(nums,0,len(nums)-1)
-        # find pivot
-        pivot = right - 1
-        successor = 0
-        # find the number(B) which is just larger than A among the numbers lying to its right section
-        for i in range(len(nums) - 1, pivot, -1):
-            if nums[i] > nums[pivot]:
-                successor = i
-                break
-        # swap pivot and successor
-        nums[pivot], nums[successor] = nums[successor], nums[pivot]  
-        # reverse non-increasing suffix
-        self.reverse(nums,pivot + 1,len(nums) - 1)
+        
+        
+        n = len(nums)
+        if n == 1:
+            return nums
+        
+        # from right to left, find first dip (decrease)
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+        if i == -1: # if increasing all the way from right to left, just reverse
+            self.reverse(nums, 0, n-1)
+            return
+        
+        
+        # find minimum larger on the right of the dip
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        
+        # swap dip and the minimum larger
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        # reverse right side of the dip
+        self.reverse(nums, i+1, n-1)
+        
+    def reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
         
     def reverse(self,nums,l,r):
         while l < r:
