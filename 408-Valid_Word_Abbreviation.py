bbb0bdab9e9e7a1d636cfd9c72be0fcca4f38b9d
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-26 10:09:04 -0700

    Updates 408-Valid_Word_Abbreviation

--- 408-Valid_Word_Abbreviation.py
+++ 408-Valid_Word_Abbreviation.py
@@ -1,58 +1,24 @@
 """
-- string
-- O(n), O(1)
-"""
-class Solution:
-    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        word = word + "a"
-        abbr = abbr + "a" # dummy, handles the case when the end letter is a digit
-        
-        word_len = len(word)
-        word_i = 0
-        count = 0
-        for c in abbr:
-            if not c.isdigit():
-                if count:
-                    word_i += count
-                    if word_i >= word_len:
-                        return False
-                    count = 0
-                if word_i >= word_len or c != word[word_i]:
-                    return False
-                word_i += 1
-            else:
-                n = ord(c) - ord('0')
-                if not count and not n: # leading 0
-                    return False
-                count = count * 10 + n
-        return word_i == word_len
-
-"""
 - two pointers
 - O(n), O(1)
 """
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        ptr1 = ptr2 = 0
+        i1 = i2 = 0
         n1, n2 = len(word), len(abbr)
         sublen = 0
-        while ptr1 < n1: # consume word
-            if sublen > 0: # consume numbers
-                sublen -= 1
-                ptr1 += 1
-            elif ptr2 == n2: # completes abbr before word is fully consumed
-                return False
-            else: # ptr2 < n2, sublen = 0
-                if not abbr[ptr2].isdigit():
-                    if word[ptr1] != abbr[ptr2]:
-                        return False
-                    ptr1 += 1
-                    ptr2 += 1
-                else: # ptr2 is digit
-                    ptr2_start = ptr2
-                    while ptr2 < n2 and abbr[ptr2].isdigit():
-                        ptr2 += 1
-                    sublen = int(abbr[ptr2_start:ptr2])
-                    if abbr[ptr2_start] == "0": # easy to miss
-                        return False
-        return ptr2 == n2 and sublen == 0 # abbr must be all consumed
+        while i1 < n1 and i2 < n2:
+            if abbr[i2].isalpha():
+                if word[i1] != abbr[i2]:
+                    return False
+                i1 += 1
+                i2 += 1
+            else:
+                if abbr[i2] == "0":
+                    return False
+                sub_len = 0
+                while i2 < n2 and abbr[i2].isdigit():
+                    sub_len = sub_len * 10 + ord(abbr[i2]) - ord("0")
+                    i2 += 1
+                i1 += sub_len
+        return i1 == n1 and i2 == n2
