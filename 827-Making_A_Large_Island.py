85584a8a1e3fa64994b1cbc2563662d781a2735c
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-26 17:47:20 -0700

    Revisits 827-Making_A_Large_Island

--- 827-Making_A_Large_Island.py
+++ 827-Making_A_Large_Island.py
@@ -6,38 +6,40 @@
 """
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        
         M, N = len(grid), len(grid[0])
         DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        index_to_area = {} # island index start at 2
+        index_to_area = {} 
         result = 0
         
-        def dfs(r, c, index):
-            grid[r][c] = index
+        def dfs(r, c, iid):
+            grid[r][c] = iid
             area = 1
             for i, j in DIR:
                 nr, nc = r + i, c + j
                 if 0 <= nr < M and 0 <= nc < N and grid[nr][nc] == 1:
-                    area += dfs(nr, nc, index)
+                    area += dfs(nr, nc, iid)
             return area
         
-        index = 2
+        res = 0
+        cur_id = 2
         for r in range(M):
             for c in range(N):
                 if grid[r][c] == 1:
-                    area = dfs(r, c, index)
-                    index_to_area[index] = area
-                    result = max(result, area) # easy to miss
-                    index += 1
-        
+                    area = dfs(r, c, cur_id)
+                    res = max(res, area) # easy to miss: all is "1"
+                    index_to_area[cur_id] = area
+                    cur_id += 1
+                    
+            
         for r in range(M):
             for c in range(N):
                 if grid[r][c] == 0:
-                    connected_index_to_area = {} # easy to miss: avoid repetitive counts if both neighbors belong to a same island
+                    area = 1
+                    iids = set() # easy to miss: avoid repetitive counts if both neighbors belong to a same island
                     for i, j in DIR:
                         nr, nc = r + i, c + j
-                        if 0 <= nr < M and 0 <= nc < N and grid[nr][nc] > 1:
-                            connected_index_to_area[grid[nr][nc]] = index_to_area[grid[nr][nc]]
-                    result = max(result, sum(connected_index_to_area.values()) + 1)
-        
-        return result
+                        if 0 <= nr < M and 0 <= nc < N and grid[nr][nc] >= 2 and grid[nr][nc] not in iids:
+                            iids.add(grid[nr][nc])
+                            area += index_to_area[grid[nr][nc]]
+                    res = max(res, area)
+        return res
