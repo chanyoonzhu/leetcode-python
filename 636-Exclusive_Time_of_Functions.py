ae82e4855d77aca9644136be97a195d60d57275e
Chanyoon Zhu <zhuchanyoon@gmail.com>
2022-03-26 18:18:27 -0700

    Revisits 636-Exclusive_Time_of_Functions

--- 636-Exclusive_Time_of_Functions.py
+++ 636-Exclusive_Time_of_Functions.py
@@ -1,39 +1,19 @@
 """
-- stack with line sweep
-- O(n), O(n)
+- clarification: function in lower stack cannot finish before function in a higher stack finishes
 """
-class Solution:
-    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        stack = []
-        result = [0] * n
-        prev_time = 0
-        for log in logs:
-            id_, type_, time = log.split(":")
-            id_, time = int(id_), int(time)
-            if type_ == "start":
-                if stack:
-                    result[stack[-1]] += time - prev_time 
-                stack.append(id_)
-                prev_time = time
-            else:
-                result[stack.pop()] += time - prev_time + 1
-                prev_time = time + 1
-        return result
-
 """
 - stack
 - O(n), O(n)
 """
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        stack = []
+        stack = [] # id, start_time
         result = [0] * n
-        prev_time = 0
         for log in logs:
             id_, type_, time = log.split(":")
             id_, time = int(id_), int(time)
             if type_ == "start":
-                stack.append([id_, time])
+                stack.append([id_, time]) 
             else:
                 _, start_time = stack.pop()
                 duration = time - start_time + 1
